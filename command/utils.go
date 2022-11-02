18556dfabb3ab9a94523eded8e9bf5f84bd020cf
Vincent Boulineau <vincent.boulineau@datadoghq.com>
2022-11-02 13:56:32 +0100

    Fix missing file

---
+++ command/utils.go
@@ -0,0 +1,10 @@
+package command
+
+import (
+	"github.com/pulumi/pulumi-command/sdk/go/command/remote"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func WaitForCloudInit(ctx *pulumi.Context, runner *Runner) (*remote.Command, error) {
+	return runner.Command(ctx, "wait-cloud-init", pulumi.String("cloud-init status --wait"), nil, nil, nil, false)
+}

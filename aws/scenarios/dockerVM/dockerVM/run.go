9ef698860a8df6d8be20800d5afb2bc63a274e6a
pducolin <paola.ducolin@datadoghq.com>
2022-11-28 16:39:43 +0100

    [dockerVM] use NewVM in DockerVM

--- aws/scenarios/dockerVM/dockerVM/run.go
+++ aws/scenarios/dockerVM/dockerVM/run.go
@@ -1,44 +1,18 @@
 package dockerVM
 
 import (
-	"github.com/DataDog/test-infra-definitions/aws"
-	"github.com/DataDog/test-infra-definitions/aws/ec2/ec2"
-	"github.com/DataDog/test-infra-definitions/command"
+	"github.com/DataDog/test-infra-definitions/aws/scenarios/vm/vm"
 	"github.com/DataDog/test-infra-definitions/datadog/agent"
-
-	"github.com/pulumi/pulumi-command/sdk/go/command/remote"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func Run(ctx *pulumi.Context) error {
-	e, err := aws.AWSEnvironment(ctx)
+	vm, err := vm.NewVM(ctx)
 	if err != nil {
 		return err
 	}
 
-	instance, conn, err := ec2.NewDefaultEC2Instance(e, ctx.Stack(), e.DefaultInstanceType())
-	if err != nil {
-		return err
-	}
-
-	if e.AgentDeploy() {
-		runner, err := command.NewRunner(ctx.Stack()+"-conn", conn, func(r *command.Runner) (*remote.Command, error) {
-			return command.WaitForCloudInit(ctx, r)
-		})
-		if err != nil {
-			return err
-		}
-
-		aptManager := command.NewAptManager(e.Ctx, runner)
-		dockerManager := command.NewDockerManager(e.Ctx, runner, aptManager)
-		_, err = agent.NewDockerAgentInstallation(e.CommonEnvironment, dockerManager, "", "")
-		if err != nil {
-			return err
-		}
-	}
-
-	e.Ctx.Export("instance-ip", instance.PrivateIp)
-	e.Ctx.Export("connection", conn)
-
-	return nil
+	// TODO add basic command to DockerManager
+	_, err = agent.NewDockerAgentInstallation(vm.Environment, vm.DockerManager, "", "")
+	return err
 }

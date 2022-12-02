71375ce7145c62bc2c8ebfc7806899979d3d7920
Usama Saqib <usamasaqib.96@live.com>
2022-12-02 14:16:12 +0100

    wip

--- aws/scenarios/libvirt/main.go
+++ aws/scenarios/libvirt/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/DataDog/test-infra-definitions/aws"
@@ -12,12 +13,37 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
-func setupLibvirtVM(ctx *pulumi.Context, libvirtUri string) error {
+var xlst = `
+<xsl:stylesheet version="1.0"
+ xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+
+ <xsl:output omit-xml-declaration="yes"/>
+
+    <xsl:template match="node()|@*">
+      <xsl:copy>
+         <xsl:apply-templates select="node()|@*"/>
+      </xsl:copy>
+    </xsl:template>
+
+    <xsl:template match="/domain/devices/controller[@type='usb']">
+            <xsl:attribute name="model">
+                    <xsl:value-of select="'none'"/>
+            </xsl:attribute>
+    </xsl:template>
+
+
+    <xsl:template match="domain/devices/graphics"/>
+    <xsl:template match="domain/devices/audio"/>
+    <xsl:template match="domain/devices/video"/>
+</xsl:stylesheet>
+`
+
+func setupLibvirtVM(ctx *pulumi.Context, libvirtUri pulumi.StringOutput, waitForList []pulumi.Resource) error {
 	// create a provider, this isn't required, but will make it easier to configure
 	// a libvirt_uri, which we'll discuss in a bit
 	provider, err := libvirt.NewProvider(ctx, "provider", &libvirt.ProviderArgs{
-		Uri: pulumi.String(libvirtUri),
-	})
+		Uri: libvirtUri,
+	}, pulumi.DependsOn(waitForList))
 	if err != nil {
 		return err
 	}
@@ -34,51 +60,42 @@ func setupLibvirtVM(ctx *pulumi.Context, libvirtUri string) error {
 		return err
 	}
 
-	// create a volume with the contents being a Ubuntu 20.04 server image
-	ubuntu, err := libvirt.NewVolume(ctx, "ubuntu", &libvirt.VolumeArgs{
-		Pool:   pool.Name,
-		Source: pulumi.String("https://cloud-images.ubuntu.com/releases/focal/release/ubuntu-20.04-server-cloudimg-amd64.img"),
-	}, pulumi.Provider(provider))
-	if err != nil {
-		return err
-	}
-
 	// create a filesystem volume for our VM
 	// This filesystem will be based on the `ubuntu` volume above
 	// we'll use a size of 10GB
 	filesystem, err := libvirt.NewVolume(ctx, "filesystem", &libvirt.VolumeArgs{
-		BaseVolumeId: ubuntu.ID(),
-		Pool:         pool.Name,
-		Size:         pulumi.Int(10000000000),
+		Pool:   pool.Name,
+		Source: pulumi.String("/tmp/pulumi-data/bullseye.qcow2"),
+		Format: pulumi.String("qcow2"),
 	}, pulumi.Provider(provider))
 	if err != nil {
 		return err
 	}
 
-	cloud_init_user_data, err := os.ReadFile("./cloud_init_user_data.yaml")
-	if err != nil {
-		return err
-	}
-
-	cloud_init_network_config, err := os.ReadFile("./cloud_init_network_config.yaml")
-	if err != nil {
-		return err
-	}
-
-	// create a cloud init disk that will setup the ubuntu credentials and enable dhcp
-	cloud_init, err := libvirt.NewCloudInitDisk(ctx, "cloud-init", &libvirt.CloudInitDiskArgs{
-		MetaData:      pulumi.String(string(cloud_init_user_data)),
-		NetworkConfig: pulumi.String(string(cloud_init_network_config)),
-		Pool:          pool.Name,
-		UserData:      pulumi.String(string(cloud_init_user_data)),
-	}, pulumi.Provider(provider))
-	if err != nil {
-		return err
-	}
+	//	cloud_init_user_data, err := os.ReadFile("./cloud_init_user_data.yaml")
+	//	if err != nil {
+	//		return err
+	//	}
+	//
+	//	cloud_init_network_config, err := os.ReadFile("./cloud_init_network_config.yaml")
+	//	if err != nil {
+	//		return err
+	//	}
+	//
+	//	// create a cloud init disk that will setup the ubuntu credentials and enable dhcp
+	//	cloud_init, err := libvirt.NewCloudInitDisk(ctx, "cloud-init", &libvirt.CloudInitDiskArgs{
+	//		MetaData:      pulumi.String(string(cloud_init_user_data)),
+	//		NetworkConfig: pulumi.String(string(cloud_init_network_config)),
+	//		Pool:          pool.Name,
+	//		UserData:      pulumi.String(string(cloud_init_user_data)),
+	//	}, pulumi.Provider(provider))
+	//	if err != nil {
+	//		return err
+	//	}
 
 	// create NAT network using 192.168.10/24 CIDR
 	network, err := libvirt.NewNetwork(ctx, "network", &libvirt.NetworkArgs{
-		Addresses: pulumi.StringArray{pulumi.String("169.254.1.0/30")},
+		Addresses: pulumi.StringArray{pulumi.String("169.254.0.2/24")},
 		Mode:      pulumi.String("nat"),
 	}, pulumi.Provider(provider))
 	if err != nil {
@@ -87,7 +104,7 @@ func setupLibvirtVM(ctx *pulumi.Context, libvirtUri string) error {
 
 	// create a VM that has a name starting with ubuntu
 	_, err = libvirt.NewDomain(ctx, "ubuntu", &libvirt.DomainArgs{
-		Cloudinit: cloud_init.ID(),
+		//	Cloudinit: cloud_init.ID(),
 		Consoles: libvirt.DomainConsoleArray{
 			// enables using `virsh console ...`
 			libvirt.DomainConsoleArgs{
@@ -106,6 +123,18 @@ func setupLibvirtVM(ctx *pulumi.Context, libvirtUri string) error {
 				NetworkId:    network.ID(),
 				WaitForLease: pulumi.Bool(false),
 			},
+		},
+		Kernel: pulumi.String("/tmp/pulumi-data/bzImage"),
+		Cmdlines: pulumi.MapArray{
+			pulumi.Map{"console": pulumi.String("ttyS0")},
+			pulumi.Map{"acpi": pulumi.String("off")},
+			pulumi.Map{"panic": pulumi.String("-1")},
+			pulumi.Map{"root": pulumi.String("/dev/sda")},
+			pulumi.Map{"net.ifnames": pulumi.String("0")},
+			pulumi.Map{"_": pulumi.String("rw")},
+		},
+		Xml: libvirt.DomainXmlArgs{
+			Xslt: pulumi.String(xlst),
 		},
 		// delete existing VM before creating replacement to avoid two VMs trying to use the same volume
 	}, pulumi.Provider(provider), pulumi.ReplaceOnChanges([]string{"*"}), pulumi.DeleteBeforeReplace(true))
@@ -157,13 +186,20 @@ func main() {
 			return err
 		}
 
-		_, err = runner.Command(e.Ctx, "restart-libvirtd", pulumi.String("sudo systemctl restart libvirtd"), nil, nil, nil, true, pulumi.DependsOn([]pulumi.Resource{disableSELinux}))
+		restart, err := runner.Command(e.Ctx, "restart-libvirtd", pulumi.String("sudo systemctl restart libvirtd"), nil, nil, nil, true, pulumi.DependsOn([]pulumi.Resource{disableSELinux}))
 		if err != nil {
 			return err
 		}
 
-		// explicitly set dependency?
-		if err := setupLibvirtVM(e.Ctx, "qemu+ssh://ubuntu@172.29.188.86/system?sshauth=privkey&keyfile=/tmp/test_rsa&known_hosts=/home/ubuntu/.ssh/known_hosts"); err != nil {
+		pubKey, err := os.ReadFile("/tmp/test_rsa.pub")
+		if err != nil {
+			return err
+		}
+
+		sshWrite, err := runner.Command(e.Ctx, "write-ssh-key", pulumi.String(fmt.Sprintf("sudo echo \"%s\" >> /home/ubuntu/.ssh/authorized_keys", string(pubKey))), nil, nil, nil, false, pulumi.DependsOn([]pulumi.Resource{instance}))
+
+		url := pulumi.Sprintf("qemu+ssh://ubuntu@%s/system?sshauth=privkey&keyfile=/tmp/test_rsa&known_hosts=/home/ubuntu/.ssh/known_hosts", instance.PrivateIp)
+		if err := setupLibvirtVM(e.Ctx, url, []pulumi.Resource{sshWrite, restart}); err != nil {
 			return err
 		}
 

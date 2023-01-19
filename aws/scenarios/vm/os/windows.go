ae6fd872ce6083504559a0e0043561a4d408e9d8
Olivier G <olivier.gaca@datadoghq.com>
2023-01-19 15:10:55 +0100

    Return an error when trying to create a Windows ARM64 VM on EC2

--- aws/scenarios/vm/os/windows.go
+++ aws/scenarios/vm/os/windows.go
@@ -1,29 +1,34 @@
 package os
 
 import (
+	"fmt"
+
 	"github.com/DataDog/test-infra-definitions/aws"
 	"github.com/DataDog/test-infra-definitions/aws/ec2/ec2"
-	oscommon "github.com/DataDog/test-infra-definitions/common/os"
+	commonos "github.com/DataDog/test-infra-definitions/common/os"
 )
 
 type windows struct {
-	*oscommon.Windows
+	*commonos.Windows
 	env aws.Environment
 }
 
 func newWindows(env aws.Environment) *windows {
 	return &windows{
-		Windows: oscommon.NewWindows(&env),
+		Windows: commonos.NewWindows(&env),
 		env:     env,
 	}
 }
 
 func (*windows) GetSSHUser() string { return "administrator" }
 
-func (w *windows) GetImage(arch oscommon.Architecture) (string, error) {
+func (w *windows) GetImage(arch commonos.Architecture) (string, error) {
+	if arch != commonos.AMD64Arch {
+		return "", fmt.Errorf("the architecture %v is not supported on Windows", arch)
+	}
 	return ec2.SearchAMI(w.env, "801119661308", "Windows_Server-2022-English-Full-Base-*", string(arch))
 }
 
-func (*windows) GetAMIArch(arch oscommon.Architecture) string { return string(arch) }
+func (*windows) GetAMIArch(arch commonos.Architecture) string { return string(arch) }
 
 func (*windows) GetTenancy() string { return "default" }

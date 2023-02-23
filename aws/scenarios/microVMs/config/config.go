8ca8efa632a4f3297bb85398d08b0060b21f62fa
usamasaqib <usamasaqib.96@live.com>
2023-02-23 16:22:11 +0100

    remove duplicated getters

--- aws/scenarios/microVMs/config/config.go
+++ aws/scenarios/microVMs/config/config.go
@@ -1,9 +1,6 @@
 package config
 
 import (
-	"errors"
-	"fmt"
-
 	"github.com/DataDog/test-infra-definitions/aws"
 	"github.com/DataDog/test-infra-definitions/aws/ec2/ec2"
 	sdkconfig "github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
@@ -35,44 +32,4 @@ func NewMicroVMConfig(e aws.Environment) DDMicroVMConfig {
 		sdkconfig.New(e.Ctx, ddMicroVMNamespace),
 		e,
 	}
-}
-
-func (e *DDMicroVMConfig) GetStringWithDefault(config *sdkconfig.Config, paramName string, defaultValue string) string {
-	val, err := config.Try(paramName)
-	if err == nil {
-		return val
-	}
-
-	if !errors.Is(err, sdkconfig.ErrMissingVar) {
-		e.Ctx.Log.Error(fmt.Sprintf("Parameter %s not parsable, err: %v, will use default value: %v", paramName, err, defaultValue), nil)
-	}
-
-	return defaultValue
-}
-
-func (e *DDMicroVMConfig) GetIntWithDefault(config *sdkconfig.Config, paramName string, defaultValue int) int {
-	val, err := config.TryInt(paramName)
-	if err == nil {
-		return val
-	}
-
-	if !errors.Is(err, sdkconfig.ErrMissingVar) {
-		e.Ctx.Log.Error(fmt.Sprintf("Parameter %s not parsable, err: %v, will use default value: %v", paramName, err, defaultValue), nil)
-	}
-
-	return defaultValue
-
-}
-
-func (e *DDMicroVMConfig) GetBoolWithDefault(config *sdkconfig.Config, paramName string, defaultValue bool) bool {
-	val, err := config.TryBool(paramName)
-	if err == nil {
-		return val
-	}
-
-	if !errors.Is(err, sdkconfig.ErrMissingVar) {
-		e.Ctx.Log.Error(fmt.Sprintf("Parameter %s not parsable, err: %v, will use default value: %v", paramName, err, defaultValue), nil)
-	}
-
-	return defaultValue
 }

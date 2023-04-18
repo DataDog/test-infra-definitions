47c6161862a9196bce5dbbf8801914d92ae78db9
Olivier G <olivier.gaca@datadoghq.com>
2023-04-18 18:25:11 +0200

    Add RemoteServiceConnector

--- common/vm/vm.go
+++ common/vm/vm.go
@@ -27,6 +27,7 @@ type genericVM struct {
 	os          commonos.OS
 	fileManager *command.FileManager
 	stackKey    string
+	*utils.RemoteServiceConnector[ClientData]
 }
 
 func NewGenericVM(
@@ -60,26 +61,21 @@ func NewGenericVM(
 	}
 
 	stackKey := fmt.Sprintf("%v-connection", name)
-	ctx.Export(stackKey, connection)
 
+	remoteServiceConnector := utils.NewRemoteServiceConnector(ctx, ClientData{})
+	remoteServiceConnector.Register(stackKey, "Connection", connection)
 	return &genericVM{
-		runner:      runner,
-		env:         commonEnv,
-		os:          os,
-		stackKey:    stackKey,
-		fileManager: command.NewFileManager(runner),
+		runner:                 runner,
+		env:                    commonEnv,
+		os:                     os,
+		stackKey:               stackKey,
+		fileManager:            command.NewFileManager(runner),
+		RemoteServiceConnector: remoteServiceConnector,
 	}, nil
 }
 
 type ClientData struct {
 	Connection utils.Connection
-}
-
-func (vm *genericVM) GetClientDataDeserializer() func(auto.UpResult) (*ClientData, error) {
-	return func(result auto.UpResult) (*ClientData, error) {
-		connection, err := utils.NewConnection(result, vm.stackKey)
-		return &ClientData{Connection: connection}, err
-	}
 }
 
 func (vm *genericVM) GetRunner() *command.Runner {

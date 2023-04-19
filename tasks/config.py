8fa1fd986459fd8037e9378429a54c447b6683b4
Olivier G <olivier.gaca@datadoghq.com>
2023-04-19 11:07:38 +0200

    Add the config option `checkKeyPair`

--- tasks/config.py
+++ tasks/config.py
@@ -4,7 +4,7 @@ import os
 import yaml
 from pathlib import Path
 from .tool import *
-from typing import List
+from typing import List, Dict, Any, Optional
 
 
 class Config:
@@ -18,13 +18,14 @@ def get_config() -> Config:
         with open(config_path) as f:
             content = f.read()
             config_dict = yaml.load(content, Loader=yaml.Loader)
-            key_name = "ddinfra:aws/defaultKeyPairName"
-            if key_name not in config_dict:
-                raise invoke.Exit(
-                    f"Cannot find the mandatory key {key_name} in {config_path}"
-                )
-            key_pair = config_dict[key_name]
-            check_key_pair(key_pair)
+            key_pair: str = _get_value(
+                "ddinfra:aws/defaultKeyPairName", config_dict, config_path
+            )
+            check_key_pair: Optional[bool] = _get_optional_value(
+                "checkKeyPair", config_dict
+            )
+            if check_key_pair is not None and check_key_pair:
+                _check_key_pair(key_pair, config_path)
 
             c = Config()
             c.key_pair = key_pair
@@ -33,14 +34,20 @@ def get_config() -> Config:
         raise invoke.Exit(f"Cannot find the configuration located at {config_path}")
 
 
-def check_key_pair(key_pair_to_search: str):
-    output = ""
-    try:
-        output = subprocess.check_output(["ssh-add", "-L"])
-    except:
-        # if ssh-add is not there don't display a warnings
-        return
+def _get_value(key: str, config_dict: Dict[str, Any], config_path: Path) -> Any:
+    if key not in config_dict:
+        raise invoke.Exit(f"Cannot find the mandatory key {key} in {config_path}")
+    return config_dict[key]
 
+
+def _get_optional_value(key: str, config_dict: Dict[str, Any]) -> Optional[Any]:
+    if key not in config_dict:
+        return None
+    return config_dict[key]
+
+
+def _check_key_pair(key_pair_to_search: str, config_path: Path):
+    output = subprocess.check_output(["ssh-add", "-L"])
     key_pairs: List[str] = []
     output = output.decode("utf-8")
     for line in output.splitlines():
@@ -51,7 +58,8 @@ def check_key_pair(key_pair_to_search: str):
             key_pairs.append(key_pair)
 
     if key_pair_to_search not in key_pairs:
-        warn(
+        raise invoke.Exit(
             f"Your key pair value '{key_pair_to_search}' is not find in ssh-agent. "
-            + f"You may have issue to connect to the remote instance. Possible values are \n{key_pairs}"
+            + f"You may have issue to connect to the remote instance. Possible values are \n{key_pairs}. "
+            + f"You can skip this check by setting `checkKeyPair: false` in {config_path}"
         )

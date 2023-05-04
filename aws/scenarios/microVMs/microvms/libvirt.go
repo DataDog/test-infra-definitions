15d726ba9455b707c133f77f584747da889fc3bc
usamasaqib <usamasaqib.96@live.com>
2023-05-04 14:48:39 +0200

    add comment explaining change of subnet

--- aws/scenarios/microVMs/microvms/libvirt.go
+++ aws/scenarios/microVMs/microvms/libvirt.go
@@ -14,6 +14,11 @@ import (
 )
 
 const (
+	// The microvm subnet changed from /16 to /24 because the underlying libvirt sdk would identify
+	// the incorrect network interface. It looks like it does not respect the subnet range when the subnet
+	// used is /16
+	// TODO: this problem only manifests when setting up VMs locally. Investigate the root cause to see what can
+	// be done. This solution may no longer work when the number of VMs exceeds the ips available in this subnet.
 	microVMGroupSubnet    = "169.254.0.0/24"
 	domainSocketCreateCmd = `rm -f /tmp/%s.sock && python3 -c "import socket as s; sock = s.socket(s.AF_UNIX); sock.bind('/tmp/%s.sock')"`
 )

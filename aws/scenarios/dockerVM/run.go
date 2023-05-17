1a36f20897ad4a5b88928c5cda8f2dd7550c7c65
pducolin <paola.ducolin@datadoghq.com>
2023-05-17 11:41:34 +0200

    [docker] fix docker scenario

--- aws/scenarios/dockerVM/run.go
+++ aws/scenarios/dockerVM/run.go
@@ -2,7 +2,6 @@ package dockervm
 
 import (
 	ec2vm "github.com/DataDog/test-infra-definitions/aws/scenarios/vm/ec2VM"
-	"github.com/DataDog/test-infra-definitions/common/config"
 	"github.com/DataDog/test-infra-definitions/datadog/agent/docker"
 
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
@@ -14,10 +13,7 @@ func Run(ctx *pulumi.Context) error {
 		return err
 	}
 
-	env, err := config.NewCommonEnvironment(ctx)
-	if err != nil {
-		return err
-	}
+	env := vm.GetCommonEnvironment()
 	var options []func(*docker.Params) error
 	if env.AgentDeploy() {
 		options = append(options, docker.WithAgent())

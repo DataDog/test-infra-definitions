700a7d3e5794e3180055d001f4815fef4e26bc83
pducolin <paola.ducolin@datadoghq.com>
2023-05-24 18:39:44 +0200

    [task] fix linter issues

--- tasks/setup.py
+++ tasks/setup.py
@@ -27,9 +27,17 @@ def setup(ctx: Context):
         
 
     if config.configParams is None:
-        config.configParams = Config.Params()
-        config.configParams.aws = Config.Params.Aws()
-        config.configParams.agent = Config.Params.Agent()
+        config.configParams = Config.Params(
+            aws=None,
+            agent=None
+        )
+    
+    # AWS config
+    if config.configParams.aws is None:
+        config.configParams.aws = Config.Params.Aws(
+            keyPairName=None,
+            publicKeyPath= None,
+        )
         
     if config.configParams.aws.keyPairName is None: 
         config.configParams.aws.keyPairName = getpass.getuser()
@@ -38,8 +46,9 @@ def setup(ctx: Context):
         config.configParams.aws.keyPairName = keyPairName
 
     if config.options is None:
-        config.options = Config.Options()
-        config.options.checkKeyPair = False
+        config.options = Config.Options(
+            checkKeyPair=False
+        )
     default = "Y" if config.options.checkKeyPair else "N"
     checkKeyPair = ask(f"Did you create your SSH key on AWS and want me to check it is loaded on your ssh agent when creating manual environments or running e2e tests [Y/N]? Default [{default}]: ")
     if len(checkKeyPair) > 0:
@@ -47,7 +56,9 @@ def setup(ctx: Context):
 
     if config.configParams.aws.publicKeyPath is None:
         config.configParams.aws.publicKeyPath = str(Path.home().joinpath(".ssh", "id_ed25519.pub").absolute())
+    default_public_key_path = config.configParams.aws.publicKeyPath
     while True:
+        config.configParams.aws.publicKeyPath = default_public_key_path
         publicKeyPath = ask(f"🔑 Path to your public ssh key, default [{config.configParams.aws.publicKeyPath}]: ")
         if len(publicKeyPath) > 0:
             config.configParams.aws.publicKeyPath = publicKeyPath
@@ -55,11 +66,19 @@ def setup(ctx: Context):
             break
         warn(f"{config.configParams.aws.publicKeyPath} is not a valid ssh key")
     
+    # Agent config
+    if config.configParams.agent is None:
+        config.configParams.agent = Config.Params.Agent(
+            apiKey=None,
+            appKey=None,
+        )
+
     if config.configParams.agent.apiKey is None:
-        config.configParams.agent.apiKey = "00000000000000000000000000000000"
+        config.configParams.agent.apiKey = '0' * 32
+    default_api_key = config.configParams.agent.apiKey
     while True:
-        config.configParams.agent.apiKey = "00000000000000000000000000000000"
-        apiKey = ask("🐶 Datadog API key - default [00000000000000000000000000000000]: ")
+        config.configParams.agent.apiKey = default_api_key
+        apiKey = ask(f"🐶 Datadog API key - default [{_get_safe_dd_key(config.configParams.agent.apiKey)}]: ")
         if len(apiKey) > 0:
             config.configParams.agent.apiKey = apiKey
         if len(config.configParams.agent.apiKey) == 32:
@@ -67,12 +86,14 @@ def setup(ctx: Context):
         warn(f"Expecting API key of length 32, got {len(config.configParams.agent.apiKey)}")
 
     if config.configParams.agent.appKey is None:    
-        config.configParams.agent.appKey = "0000000000000000000000000000000000000000"
+        config.configParams.agent.appKey = '0' * 40
+    default_app_key = config.configParams.agent.appKey
     while True:
-        config.configParams.agent.appKey = "0000000000000000000000000000000000000000"
-        appKey = ask("🐶 Datadog APP key - default [0000000000000000000000000000000000000000]: ")
-        if len(appKey) > 0:
-            config.configParams.agent.appKey = appKey
+        config.configParams.agent.appKey = default_app_key
+
+        app_Key = ask(f"🐶 Datadog APP key - default [{_get_safe_dd_key(config.configParams.agent.appKey)}]: ")
+        if len(app_Key) > 0:
+            config.configParams.agent.appKey = app_Key
         if len(config.configParams.agent.appKey) == 40:
             break
         warn(f"Expecting APP key of length 40, got {len(config.configParams.agent.appKey)}")
@@ -85,7 +106,10 @@ def setup(ctx: Context):
     )
     
       
-    
+def _get_safe_dd_key(key: str) -> str: 
+    if key == '0' * len(key):
+        return key
+    return '*' * len(key)   
 
     
 

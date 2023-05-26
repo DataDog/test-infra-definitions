0ad661478770438da96b2eb919a879e2506a4b57
Olivier G <olivier.gaca@datadoghq.com>
2023-05-26 11:09:18 +0200

    Improve destroy task calling `inv create-vm` and `inv destroy-vm` destroy correctly the vm

--- tasks/destroy.py
+++ tasks/destroy.py
@@ -1,6 +1,6 @@
 import subprocess
 from .tool import get_stack_name, get_stack_name_prefix, info, error
-from typing import Optional, List
+from typing import Optional, List, Tuple
 
 
 
@@ -9,28 +9,29 @@ def destroy(scenario_name: str, stack: Optional[str] = None):
     Destroy an environment
     """
 
-    stack_name = get_stack_name(stack, scenario_name)
+    full_stack_name = get_stack_name(stack, scenario_name)
+    short_stack_names, full_stack_names = _get_existing_stacks()
 
-    stacks = _get_existing_stacks()
-
-    if len(stacks) == 0:
+    if len(short_stack_names) == 0:
         info("No stack to destroy")
         return
 
-    # if there is a single stack delete it
-    if stack is None and len(stacks) == 1:
-        stack = stacks[0]
+    if stack is not None:
+        if stack in short_stack_names:
+            full_stack_name = f"{get_stack_name_prefix()}{stack}"
+        else:
+            error(f"Unknown stack '{stack}'")
+            full_stack_name = None            
+    else: 
+        if full_stack_name not in full_stack_names:
+            error(f"Unknown stack '{full_stack_name}'")
+            full_stack_name = None
 
-    if stack is not None and stack not in stacks:
-        error(f"Unknown stack '{stack}'")
-        stack = None
-
-    if stack is None:
+    if full_stack_name is None:
         error("Run this command with '--stack-name MY_STACK_NAME'. Available stacks are:")
-        for stack_name in stacks:
+        for stack_name in short_stack_names:
             error(f" {stack_name}")
-    else:
-        stack = f"{get_stack_name_prefix()}{stack}"
+    else:        
         subprocess.call(
             [
                 "aws-vault",
@@ -41,21 +42,23 @@ def destroy(scenario_name: str, stack: Optional[str] = None):
                 "destroy",
                 "--remove",
                 "-s",
-                stack,
+                full_stack_name,
             ]
         )
 
 
-def _get_existing_stacks() -> List[str]:
+def _get_existing_stacks() -> Tuple[List[str], List[str]]:
     output = subprocess.check_output(["pulumi", "stack", "ls", "--all"])
     output = output.decode("utf-8")
     lines = output.splitlines()
     lines = lines[1:]  # skip headers
     stacks: List[str] = []
+    full_stacks: List[str] = []
     stack_name_prefix = get_stack_name_prefix()
     for line in lines:
         stack_name = line.split(" ")[0]
         if stack_name.startswith(stack_name_prefix):
+            full_stacks.append(stack_name)
             stack_name = stack_name[len(stack_name_prefix):]
             stacks.append(stack_name)
-    return stacks
+    return stacks, full_stacks

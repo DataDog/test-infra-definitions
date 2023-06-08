8fd5a6314a9ea6e4de3d3cc4d9f23f952951fd23
amit.slavin <amit.slavin@datadoghq.com>
2023-06-08 10:13:25 +0300

    fixed the ut name

--- components/datadog/agent/params_test.go
+++ components/datadog/agent/params_test.go
@@ -26,7 +26,7 @@ func TestParams(t *testing.T) {
 			BetaChannel: true,
 		})
 	})
-	t.Run("parseVersion should correctly parse custom agent version", func(t *testing.T) {
+	t.Run("parsePipelineVersion should correctly parse a pipeline ID and format the agent version pipeline", func(t *testing.T) {
 		version := parsePipelineVersion("16362517")
 		assert.Equal(t, version, os.AgentVersion{
 			PipelineID: "pipeline-16362517",

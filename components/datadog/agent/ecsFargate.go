221678e6a43d4aa54ecf32a996c526c87cbf2374
Kevin Fairise <kevin.fairise@datadoghq.com>
2023-12-11 09:56:07 +0100

    Add image params to ECSFargateLinuxContainer

--- components/datadog/agent/ecsFargate.go
+++ components/datadog/agent/ecsFargate.go
@@ -8,54 +8,20 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
-func ECSFargateLinuxContainerDefinition(e config.CommonEnvironment, apiKeySSMParamName pulumi.StringInput, fakeintake *ddfakeintake.ConnectionExporter, logConfig ecs.TaskDefinitionLogConfigurationPtrInput) *ecs.TaskDefinitionContainerDefinitionArgs {
+func ECSFargateLinuxContainerDefinition(e config.CommonEnvironment, image string, apiKeySSMParamName pulumi.StringInput, fakeintake *ddfakeintake.ConnectionExporter, logConfig ecs.TaskDefinitionLogConfigurationPtrInput) *ecs.TaskDefinitionContainerDefinitionArgs {
 
-	return &ecs.TaskDefinitionContainerDefinitionArgs{
-		Cpu:       pulumi.IntPtr(0),
-		Name:      pulumi.String("datadog-agent"),
-		Image:     pulumi.Sprintf(DockerAgentFullImagePath(&e, "public.ecr.aws/datadog/agent")),
-		Essential: pulumi.BoolPtr(true),
-		Environment: append(ecs.TaskDefinitionKeyValuePairArray{
-			ecs.TaskDefinitionKeyValuePairArgs{
-				Name:  pulumi.StringPtr("DD_DOGSTATSD_SOCKET"),
-				Value: pulumi.StringPtr("/var/run/datadog/dsd.socket"),
-			},
-			ecs.TaskDefinitionKeyValuePairArgs{
-				Name:  pulumi.StringPtr("ECS_FARGATE"),
-				Value: pulumi.StringPtr("true"),
-			},
-		}, ecsFakeintakeAdditionalEndpointsEnv(fakeintake)...),
-		Secrets: ecs.TaskDefinitionSecretArray{
-			ecs.TaskDefinitionSecretArgs{
-				Name:      pulumi.String("DD_API_KEY"),
-				ValueFrom: apiKeySSMParamName,
-			},
-		},
-		MountPoints: ecs.TaskDefinitionMountPointArray{
-			ecs.TaskDefinitionMountPointArgs{
-				ContainerPath: pulumi.StringPtr("/var/run/datadog"),
-				SourceVolume:  pulumi.StringPtr("dd-sockets"),
-			},
-		},
-		HealthCheck: &ecs.TaskDefinitionHealthCheckArgs{
-			Retries:     pulumi.IntPtr(2),
-			Command:     pulumi.ToStringArray([]string{"CMD-SHELL", "/probe.sh"}),
-			StartPeriod: pulumi.IntPtr(10),
-			Interval:    pulumi.IntPtr(30),
-			Timeout:     pulumi.IntPtr(5),
-		},
-		LogConfiguration: logConfig,
-		PortMappings:     ecs.TaskDefinitionPortMappingArray{},
-		VolumesFrom:      ecs.TaskDefinitionVolumeFromArray{},
+	var agentImage string
+
+	if image == "" {
+		agentImage = DockerAgentFullImagePath(&e, "public.ecr.aws/datadog/agent")
+	} else {
+		agentImage = image
 	}
-}
-
-func ECSFargateLinuxContainerDefinitionLatest(apiKeySSMParamName pulumi.StringInput, fakeintake *ddfakeintake.ConnectionExporter, logConfig ecs.TaskDefinitionLogConfigurationPtrInput) *ecs.TaskDefinitionContainerDefinitionArgs {
 
 	return &ecs.TaskDefinitionContainerDefinitionArgs{
 		Cpu:       pulumi.IntPtr(0),
 		Name:      pulumi.String("datadog-agent"),
-		Image:     pulumi.Sprintf("public.ecr.aws/datadog/agent:latest"),
+		Image:     pulumi.Sprintf(agentImage),
 		Essential: pulumi.BoolPtr(true),
 		Environment: append(ecs.TaskDefinitionKeyValuePairArray{
 			ecs.TaskDefinitionKeyValuePairArgs{

c88a7d5261b6c46e240122302936d94e6e7e3109
Kyle Nusbaum <kyle@datadog.com>
2024-01-22 14:22:47 -0600

    components/datadog/apps: add tracegen application

---
+++ components/datadog/apps/tracegen/images/tracegen/main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"math"
+	"sync/atomic"
+	"time"
+
+	"golang.org/x/time/rate"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
+)
+
+var tracecount atomic.Uint32
+var spancount atomic.Uint32
+
+func reportStats() {
+	for {
+		time.Sleep(5 * time.Second)
+		tc := tracecount.Swap(0)
+		sc := spancount.Swap(0)
+		fmt.Printf("Finished %d traces/s, %d spans/second.\n", tc/5, sc/5)
+	}
+}
+
+func main() {
+	tps := flag.Float64("tps", 1, "Target number of traces to generate per second.")
+	spt := flag.Uint("spt", 2, "Number of spans to put in each trace. (>=1)")
+	testDuration := flag.Duration("testtime", 0, "Amount of time to run the test. A value of '0' means the test will continue indefinitely.")
+	flag.Parse()
+
+	tracer.Start()
+	defer tracer.Stop()
+
+	// Sleeping is expensive and inaccurate, so for high trace/s rates, we need to send bursts.
+	// Instead of send, sleep, send, sleep, etc. we will calculate the number of traces to send
+	// per 100ms, and send that many traces every 100ms using the rate.Limiter.
+	// 100ms seems to be a good period allowing us to spread trace generation while also
+	// remaining accurate up to very high trace/s rates
+
+	// tperloop is the max traces we can send per loop iteration (every ~1/10th of a second - 100 ms).
+	// The rate limiter helpfully adjusts the timing to give exactly tps, at tperloop
+	// per iteration. ceil makes sure our period is always >= 100ms
+	tperloop := int(math.Ceil(*tps / 10))
+	lim := rate.NewLimiter(rate.Limit(*tps), tperloop)
+
+	testStart := time.Now()
+	go reportStats()
+	fmt.Printf("Sending %v Traces/s, each with %d spans.\n", *tps, *spt)
+	for i := 0; ; i++ {
+		istart := time.Now()
+		if *testDuration > 0 && istart.After(testStart.Add(*testDuration)) {
+			return
+		}
+		lim.WaitN(context.Background(), tperloop)
+		for sel := 0; sel < tperloop; sel++ {
+			switch sel % 2 {
+			case 0:
+				genChain(*spt)
+			case 1:
+				genFlat(*spt)
+			}
+		}
+	}
+}
+
+// genChain generates a trace with spans count of spans in it.
+// The trace is structured with each span N being the child of span N-1.
+func genChain(spans uint) {
+	sp := tracer.StartSpan("tracegen_chain")
+	for i := uint(1); i < spans; i++ {
+		defer sp.Finish()
+		spancount.Add(1)
+		sp = tracer.StartSpan(fmt.Sprintf("tracegen_chain(%d)", i),
+			tracer.ChildOf(sp.Context()))
+	}
+	sp.Finish()
+	spancount.Add(1)
+	tracecount.Add(1)
+}
+
+// genFlat generates a trace with spans count of spans in it.
+// The trace is structured with one root span, with all other spans being
+// children of that root.
+func genFlat(spans uint) {
+	const traceDuration = 1 * time.Second
+	tdelta := traceDuration / time.Duration(spans) // Duration of each child span
+	start := time.Now()
+	root := tracer.StartSpan("tracegen_flat")
+	defer func() {
+		root.Finish(tracer.FinishTime(start.Add(traceDuration)))
+		spancount.Add(1)
+		tracecount.Add(1)
+	}()
+	for i := uint(1); i < spans; i++ {
+		sp := tracer.StartSpan(fmt.Sprintf("tracegen_flat(%d)", i),
+			tracer.StartTime(start.Add(tdelta*time.Duration(i))),
+			tracer.ChildOf(root.Context()))
+		sp.Finish(tracer.FinishTime(start.Add(tdelta*time.Duration(i) + tdelta)))
+		spancount.Add(1)
+	}
+}

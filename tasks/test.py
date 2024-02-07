f394ed391bd6c328ec24173e390c6fc802846a69
Guillermo Julián <guillermo.julian@datadoghq.com>
2024-02-07 13:28:54 +0100

    Improve check-xslt task

--- tasks/test.py
+++ tasks/test.py
@@ -1,3 +1,4 @@
+import os
 from invoke.tasks import task
 from invoke.exceptions import Exit
 import difflib
@@ -13,55 +14,30 @@ except ImportError:
     help={
         "xslt": "XSLT file to use for transformation",
         "replacements": "Override replacements to use for transformation. Multiple arguments of the form key=value",
+        "xml": "File to use as a base for transformation. This can be one of the names in the tasks/microvm-sample-xmls directory (e.g., domain, network, volume) or a path to an XML file.",
     },
 )
-def check_xslt(ctx, xslt, replacements=None):
+def check_xslt(ctx, xslt, replacements=None, xml="domain"):
     """
     Checks the XSLT transformations in the scenarios/aws/microVMs/microvms/resources path
     Useful for testing and checking transformation there without running the full pulumi stack
     """
-    base_xml = """
-<domain type="kvm">
-    <name>local-ddvm-local-ubuntu_22.04-distro_local-ddvm-4-8192</name>
-    <memory unit="MiB">8192</memory>
-    <vcpu>4</vcpu>
-    <os firmware="efi">
-        <type machine="virt">hvm</type>
-    </os>
-    <features>
-        <pae></pae>
-        <acpi></acpi>
-        <apic></apic>
-    </features>
-    <cpu></cpu>
-    <devices>
-        <disk type="volume" device="disk">
-            <driver name="qemu" type="qcow2"></driver>
-            <source pool="local-ddvm-global-pool" volume="local-ddvm-global-pool-ubuntu_22.04-distro_local-local-final-overlay-ubuntu_22.04-05d642c"></source>
-            <target dev="vda" bus="virtio"></target>
-        </disk>
-        <disk type="volume" device="disk">
-            <driver name="qemu" type="qcow2"></driver>
-            <source pool="local-ddvm-global-pool" volume="local-ddvm-global-pool-docker-arm64.qcow2-distro_local-local-final-overlay-ubuntu_22.04-9c703cf"></source>
-            <target dev="vdb" bus="virtio"></target>
-        </disk>
-        <console>
-            <target type="serial" port="0"></target>
-        </console>
-        <channel type="unix">
-            <target type="virtio" name="org.qemu.guest_agent.0"></target>
-        </channel>
-        <rng model="virtio">
-            <backend model="random">/dev/urandom</backend>
-        </rng>
-    </devices>
-</domain>
-"""
-
     try:
         import lxml.etree as etree
     except ImportError:
         raise Exit("lxml is not installed. Please install it with `pip install lxml`")
+
+    if os.path.exists(xml):
+        xml_path = xml
+    elif os.path.exists(f"tasks/microvm-sample-xmls/{xml}.xml"):
+        xml_path = f"tasks/microvm-sample-xmls/{xml}.xml"
+    elif os.path.exists(f"tasks/microvm-sample-xmls/{xml}"):
+        xml_path = f"tasks/microvm-sample-xmls/{xml}"
+    else:
+        raise Exit(f"Could not find XML file {xml}")
+
+    with open(xml_path, "r") as f:
+        base_xml = f.read()
 
     parser = etree.XMLParser(remove_blank_text=True)
     dom = etree.fromstring(base_xml, parser)

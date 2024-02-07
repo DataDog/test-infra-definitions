ef0d43efdeeb543832695cc06944585deb5f07e9
Kevin Fairise <132568982+KevinFairise2@users.noreply.github.com>
2024-02-07 18:00:54 +0100

    Update components/datadog/dogstatsd-standalone/docker_image.go
    Co-authored-by: pducolin <45568537+pducolin@users.noreply.github.com>

--- components/datadog/dogstatsd-standalone/docker_image.go
+++ components/datadog/dogstatsd-standalone/docker_image.go
@@ -20,7 +20,7 @@ func dockerDogstatsdFullImagePath(e *config.CommonEnvironment, repositoryPath st
 
 	// if agent pipeline id and commit sha are defined, use the image from the pipeline pushed on agent QA registry
 	if e.PipelineID() != "" && e.CommitSHA() != "" {
-		return utils.BuildDockerImagePath("669783387624.dkr.ecr.us-east-1.amazonaws.com/cluster-agent", fmt.Sprintf("%s-%s", e.PipelineID(), e.CommitSHA()))
+		return utils.BuildDockerImagePath("669783387624.dkr.ecr.us-east-1.amazonaws.com/dogstatsd", fmt.Sprintf("%s-%s", e.PipelineID(), e.CommitSHA()))
 	}
 
 	if repositoryPath == "" {

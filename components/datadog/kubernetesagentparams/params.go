f9580628c451fa5a22fe070be842cdd8ee2687ff
Kevin Fairise <kevin.fairise@datadoghq.com>
2024-02-13 14:50:14 +0100

    Update doc

--- components/datadog/kubernetesagentparams/params.go
+++ components/datadog/kubernetesagentparams/params.go
@@ -23,12 +23,11 @@ const (
 //   - [WithRepository]
 //   - [WithFullImagePath]
 //   - [WithPulumiDependsOn]
-//   - [WithEnvironmentVariables]
-//   - [WithAgentServiceEnvVariable]
+//   - [WithDeployWindows]
+//   - [WithHelmValues]
+//   - [WithNamespace]
 //   - [WithHostName]
 //	 - [WithFakeintake]
-//	 - [WithLogs]
-//   - [WithExtraComposeManifest]
 //
 // [Functional options pattern]: https://dave.cheney.net/2014/10/17/functional-options-for-friendly-apis
 

2bdbdc0ae405bdd54a07c5381adb24b7bb8ee25d
Kevin Fairise <132568982+KevinFairise2@users.noreply.github.com>
2024-02-21 18:30:35 +0100

    Update components/datadog/kubernetesagentparams/params.go
    Co-authored-by: Vincent Boulineau <58430298+vboulineau@users.noreply.github.com>

--- components/datadog/kubernetesagentparams/params.go
+++ components/datadog/kubernetesagentparams/params.go
@@ -26,7 +26,7 @@ const (
 //   - [WithHelmValues]
 //   - [WithNamespace]
 //   - [WithDeployWindows]
-//	 - [WithFakeintake]
+//   - [WithFakeintake]
 //
 // [Functional options pattern]: https://dave.cheney.net/2014/10/17/functional-options-for-friendly-apis
 

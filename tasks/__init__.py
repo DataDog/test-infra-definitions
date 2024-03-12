608e58bce480e41f45f254f59f1fd4881aa157ac
pducolin <paola.ducolin@datadoghq.com>
2024-03-12 10:23:03 +0100

    [kind] add invoke task to create a kind environment

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -5,6 +5,7 @@ from tasks.aks import create_aks, destroy_aks
 from tasks.docker import create_docker, destroy_docker
 from tasks.ecs import create_ecs, destroy_ecs
 from tasks.eks import create_eks, destroy_eks
+from tasks.kind import create_kind, destroy_kind
 
 from .vm import create_vm, destroy_vm
 
@@ -19,4 +20,6 @@ ns.add_task(create_aks)  # pyright: ignore [reportArgumentType]
 ns.add_task(destroy_aks)  # pyright: ignore [reportArgumentType]
 ns.add_task(create_ecs)  # pyright: ignore [reportArgumentType]
 ns.add_task(destroy_ecs)  # pyright: ignore [reportArgumentType]
+ns.add_task(create_kind)  # pyright: ignore [reportArgumentType]
+ns.add_task(destroy_kind)  # pyright: ignore [reportArgumentType]
 ns.add_collection(setup)  # pyright: ignore [reportArgumentType]

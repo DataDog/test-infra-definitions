3a97f28098936845a9c9ae5367a3b4fd2209729d
Celian Raimbault <celian.raimbault@datadoghq.com>
2024-03-15 17:11:36 +0100

    [create-vm-verify-pipeline] Formatted

--- tasks/doc.py
+++ tasks/doc.py
@@ -9,12 +9,13 @@ pipeline_id: str = (
 job_name: str = "Name of the job within the agent pipeline for example 'deploy_deb_testing-a7_x64'"
 agent_version: str = "The version of the Agent for example '7.42.0~rc.1-1' or '6.39.0 (default `latest`)'"
 container_agent_version: str = "The container version of the Agent for example '7.45.0-rc.3' (default `latest`)'"
-stack_name: str = "An optional name for the stack. This parameter is useful when you need to create several environments. Note: 'invoke destroy' may not work properly"
-debug: str = "Launch pulumi with debug mode. Default False"
 stack_name: str = (
-    "An optional name for the stack. This parameter is useful when you need to create several environments."
+    "An optional name for the stack. This parameter is useful when you need to create several environments. Note: 'invoke destroy' may not work properly"
 )
-os_family: str = f"The operating system. Possible values are {tool.get_os_families()}. Default '{tool.get_default_os_family()}'"
+debug: str = "Launch pulumi with debug mode. Default False"
+os_family: str = (
+    f"The operating system. Possible values are {tool.get_os_families()}. Default '{tool.get_default_os_family()}'"
+)
 linux_node_group: str = "Install a Linux node group (default True)"
 linux_arm_node_group: str = "Install a Linux ARM node group (default False)"
 bottlerocket_node_group: str = "Install a bottlerocket node group (default True)"

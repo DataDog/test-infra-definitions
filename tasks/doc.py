c932d9823214288be2fb3d19b73348d9e3e31a2b
Celian Raimbault <celian.raimbault@datadoghq.com>
2024-03-15 17:13:18 +0100

    [create-vm-verify-pipeline] Formatted

--- tasks/doc.py
+++ tasks/doc.py
@@ -9,9 +9,7 @@ pipeline_id: str = (
 job_name: str = "Name of the job within the agent pipeline for example 'deploy_deb_testing-a7_x64'"
 agent_version: str = "The version of the Agent for example '7.42.0~rc.1-1' or '6.39.0 (default `latest`)'"
 container_agent_version: str = "The container version of the Agent for example '7.45.0-rc.3' (default `latest`)'"
-stack_name: str = (
-    "An optional name for the stack. This parameter is useful when you need to create several environments. Note: 'invoke destroy' may not work properly"
-)
+stack_name: str = "An optional name for the stack. This parameter is useful when you need to create several environments. Note: 'invoke destroy' may not work properly"
 debug: str = "Launch pulumi with debug mode. Default False"
 os_family: str = (
     f"The operating system. Possible values are {tool.get_os_families()}. Default '{tool.get_default_os_family()}'"
@@ -23,9 +21,7 @@ windows_node_group: str = "Install a Windows node group (default False)"
 use_fargate: str = "Use Fargate (default True)"
 fakeintake: str = "Use a dedicated fake Datadog intake (default False)"
 ami_id: str = "A full Amazon Machine Image (AMI) id (e.g. ami-0123456789abcdef0)"
-architecture: str = (
-    f"The architecture to use. Possible values are {tool.get_architectures()}. Default '{tool.get_default_architecture()}'"
-)
+architecture: str = f"The architecture to use. Possible values are {tool.get_architectures()}. Default '{tool.get_default_architecture()}'"
 yes: str = "Automatically approve and perform the destroy without previewing it"
 use_aws_vault: str = "Wrap aws command with aws-vault, default to True"
 interactive: str = "Enable interactive mode, if set to False notifications and copy to clipboard are disabled"

e634eb02eaaba3df96040ce62bf6b69fbff4f554
AliDatadog <ali.benabdallah@datadoghq.com>
2024-03-28 11:26:46 +0100

    use built-in fakeintake

--- scenarios/aws/fakeintake/params.go
+++ scenarios/aws/fakeintake/params.go
@@ -15,7 +15,7 @@ type Option = func(*Params) error
 func NewParams(options ...Option) (*Params, error) {
 	params := &Params{
 		LoadBalancerEnabled: false,
-		ImageURL:            "public.ecr.aws/datadog/fakeintake:latest",
+		ImageURL:            "docker.io/alidatadog/fakeintake:sql",
 		CPU:                 512,
 		Memory:              1024,
 	}

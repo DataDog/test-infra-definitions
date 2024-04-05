a62a0082b2c0edba16825f27f3bf1b7cdea9001d
Guillermo Julián <guillermo.julian@datadoghq.com>
2024-04-05 10:14:10 +0200

    Ensure docker config folder exists

--- scenarios/aws/microVMs/microvms/provision.go
+++ scenarios/aws/microVMs/microvms/provision.go
@@ -126,7 +126,7 @@ func setDockerDataRoot(runner *Runner, disks []resources.DomainDisk, namer namer
 		}
 
 		args := command.Args{
-			Create: pulumi.Sprintf("echo -e '{\n\t\"data-root\":\"%s\"\n}' > /etc/docker/daemon.json && sudo systemctl restart docker", d.Mountpoint),
+			Create: pulumi.Sprintf("mkdir -p /etc/docker && echo -e '{\n\t\"data-root\":\"%s\"\n}' > /etc/docker/daemon.json && sudo systemctl restart docker", d.Mountpoint),
 			Sudo:   true,
 		}
 		done, err := runner.Command(namer.ResourceName("set-docker-data-root"), &args, pulumi.DependsOn(depends))

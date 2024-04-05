cb60e429d6c7b08577db4518307e78825666532c
Kevin Fairise <kevin.fairise@datadoghq.com>
2024-04-05 14:24:10 +0200

    Too much quotes

--- tasks/ci.py
+++ tasks/ci.py
@@ -44,7 +44,7 @@ Here is the full changelog between the two commits: https://github.com/DataDog/t
         pr_commit_sha = pr_commit_sha_match.group(1)
         res = ctx.run(f'git merge-base --is-ancestor {pr_commit_sha} {new_commit_sha}', warn=True, hide="both")
         if res.exited != 0:
-            print (f"Commit {pr_commit_sha} is not considered stale, skipping...")"
+            print (f"Commit {pr_commit_sha} is not considered stale, skipping...")
             continue
         reviews = pr.get_reviews()
         if reviews.totalCount != 0:

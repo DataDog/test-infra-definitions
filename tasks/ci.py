dfef348e4394b0b6ddef1699edbf490161368369
Kevin Fairise <kevin.fairise@datadoghq.com>
2024-04-05 14:37:15 +0200

    Fix CI rules and delete Github workflow

--- tasks/ci.py
+++ tasks/ci.py
@@ -10,6 +10,7 @@ from termcolor import colored
 
 
 COMMIT_TITLE_REGEX = re.compile(r"\[test-infra-definitions\]\[automated\] Bump test-infra-definitions to ([a-z0-9]*)")
+
 @task
 def create_pr_and_close_stale_ones(ctx, branch: str, new_commit_sha: str, old_commit_sha: str):
     # if os.getenv("CI") != "true":
@@ -32,14 +33,17 @@ Here is the full changelog between the two commits: https://github.com/DataDog/t
 
     new_pr =repo.create_pull(title=f"[test-infra-definitions][automated] Bump test-infra-definitions to {new_commit_sha}", body=pr_body, head=branch, base="main", draft=True)
     new_pr.add_to_labels("qa/no-code-change", "changelog/no-changelog", "automatic/test-infra-bump")
+
     print(f"PR created: {new_pr.html_url}")
+
     print("Closing stale auto bump PRs...")
+
     issues = repo.get_issues(state="open", labels=["automatic/test-infra-bump"])
     prs = [issue.as_pull_request() for issue in issues if issue.pull_request is not None and issue.number != new_pr.number]
     for pr in prs:
         pr_commit_sha_match = re.search(COMMIT_TITLE_REGEX, pr.title)
         if pr_commit_sha_match is None:
-            print("No commit sha found in PR title")
+            print(f"No commit sha found in PR title:", pr.html_url)
             continue
         pr_commit_sha = pr_commit_sha_match.group(1)
         res = ctx.run(f'git merge-base --is-ancestor {pr_commit_sha} {new_commit_sha}', warn=True, hide="both")

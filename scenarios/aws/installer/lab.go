d5ce5c24be4374315fc6225fa4316cde171fd89c
Paul Coignet <paul.coignet@datadoghq.com>
2024-04-09 15:05:01 +0200

    Bootstrap updater lab

---
+++ scenarios/aws/installer/lab.go
@@ -0,0 +1,114 @@
+package installer
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/DataDog/test-infra-definitions/components/command"
+	"github.com/DataDog/test-infra-definitions/components/datadog/updater"
+	"github.com/DataDog/test-infra-definitions/components/os"
+	"github.com/DataDog/test-infra-definitions/resources/aws"
+	"github.com/DataDog/test-infra-definitions/scenarios/aws/ec2"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+type installerLabVMArgs struct {
+	name         string
+	descriptor   os.Descriptor
+	instanceType string
+	packageNames []string
+}
+
+var installerLabVMs = []installerLabVMArgs{
+	{
+		name:         "ubuntu-22-arm64",
+		descriptor:   os.NewDescriptorWithArch(os.Ubuntu, "22.04", os.ARM64Arch),
+		instanceType: "t4g.medium",
+		packageNames: []string{
+			"datadog-agent",
+		},
+	},
+	{
+		name:         "ubuntu-22-amd64",
+		descriptor:   os.NewDescriptorWithArch(os.Ubuntu, "22.04", os.AMD64Arch),
+		instanceType: "t4g.medium",
+		packageNames: []string{
+			"datadog-agent",
+		},
+	},
+	{
+		name:         "debian-12",
+		descriptor:   os.Debian12,
+		instanceType: "t4g.medium",
+		packageNames: []string{
+			"datadog-agent",
+		},
+	},
+	{
+		name:         "amazon-linux-2023",
+		descriptor:   os.AmazonLinux2023,
+		instanceType: "t4g.medium",
+		packageNames: []string{
+			"datadog-agent",
+		},
+	},
+	{
+		name:         "centos-7",
+		descriptor:   os.CentOS7,
+		instanceType: "t4g.medium",
+		packageNames: []string{
+			"datadog-agent",
+		},
+	},
+}
+
+const installerPath = "/opt/datadog-packages/installer_boot/bin/updater/updater"
+
+func Run(ctx *pulumi.Context) error {
+	env, err := aws.NewEnvironment(ctx)
+	if err != nil {
+		return err
+	}
+
+	for _, vmArgs := range installerLabVMs {
+		vm, err := ec2.NewVM(
+			env,
+			vmArgs.name,
+			ec2.WithAMI(env.InfraOSImageID(), vmArgs.descriptor, vmArgs.descriptor.Architecture),
+			ec2.WithInstanceType(vmArgs.instanceType),
+		)
+		if err != nil {
+			return err
+		}
+		if err := vm.Export(ctx, nil); err != nil {
+			return err
+		}
+
+		// Install the installer
+		_, err = updater.NewHostUpdater(env.CommonEnvironment, vm)
+		if err != nil {
+			return err
+		}
+
+		// Bootstrap the packages
+		bootstrapCommand := []string{}
+		for _, pkg := range vmArgs.packageNames {
+			bootstrapCommand = append(
+				bootstrapCommand,
+				fmt.Sprintf("%s bootstrap -P %s", installerPath, pkg),
+			)
+		}
+
+		_, err = vm.OS.Runner().Command(
+			env.CommonNamer.ResourceName("bootstrap"),
+			&command.Args{
+				Create: pulumi.Sprintf("bash -c %s", strings.Join(bootstrapCommand, " && ")),
+			},
+		)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}

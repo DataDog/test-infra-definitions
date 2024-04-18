e71076e58046b39f765eb8ea842baddf4d248937
Nicolas Guerguadj <nicolas.guerguadj@datadoghq.com>
2024-04-18 16:42:38 +0200

    feat: add option to remove default permissions
    Signed-off-by: Nicolas Guerguadj <nicolas.guerguadj@datadoghq.com>

--- components/datadog/agentparams/filepermissions/permissions_win.go
+++ components/datadog/agentparams/filepermissions/permissions_win.go
@@ -11,7 +11,10 @@ type WindowsPermissionsOption = func(*WindowsPermissions) error
 
 // WindowsPermissions contains the information to configure the permissions of a file on Windows.
 type WindowsPermissions struct {
-	// If you are familiar with the icacls command, you can provide a custom command directly. This has precedence over any other option.
+	// RemoveDefaultPermissions removes the default permissions from the file. This is useful when you want to set permissions for secrets management.
+	RemoveDefaultPermissions bool
+
+	// If you are familiar with the icacls command, you can provide a custom command directly.
 	IcaclsCommand string
 }
 
@@ -29,10 +32,14 @@ func NewWindowsPermissions(options ...WindowsPermissionsOption) optional.Option[
 
 // SetupPermissionsCommand returns a command that sets the permissions of a file. It relies on the icacls command.
 func (p *WindowsPermissions) SetupPermissionsCommand(path string) string {
-	if p.IcaclsCommand != "" {
-		return fmt.Sprintf("icacls “%v” %v", path, p.IcaclsCommand)
+	cmd := ""
+	if p.RemoveDefaultPermissions {
+		cmd = fmt.Sprintf(`icacls "%v" /remove (Get-LocalUser) "Everyone" "Users" "Administrators" "SYSTEM" "Authenticated Users" "CREATOR OWNER" /inheritance:r /t /c /l;`, path)
 	}
-	return ""
+	if p.IcaclsCommand != "" {
+		return fmt.Sprintf(`%v icacls "%v" %v`, cmd, path, p.IcaclsCommand)
+	}
+	return cmd
 }
 
 // ResetPermissionsCommand returns a command that resets the owner, group, and permissions of a file to default.
@@ -40,10 +47,18 @@ func (p *WindowsPermissions) ResetPermissionsCommand(path string) string {
 	return fmt.Sprintf("icacls “%v” /reset /t /c /l", path)
 }
 
-// WithIcaclsCommand sets the icacls command to use. This has precedence over any other option.
+// WithIcaclsCommand sets the icacls command to use.
 func WithIcaclsCommand(command string) WindowsPermissionsOption {
 	return func(p *WindowsPermissions) error {
 		p.IcaclsCommand = command
+		return nil
+	}
+}
+
+// WithRemoveDefaultPermissions removes the default permissions from the file.
+func WithRemoveDefaultPermissions() WindowsPermissionsOption {
+	return func(p *WindowsPermissions) error {
+		p.RemoveDefaultPermissions = true
 		return nil
 	}
 }

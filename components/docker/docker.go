880f0a930e2162db6c69046431931ce01ce28835
Kevin Fairise <132568982+KevinFairise2@users.noreply.github.com>
2024-05-02 10:09:35 +0200

    Update components/docker/docker.go

--- components/docker/docker.go
+++ components/docker/docker.go
@@ -110,7 +110,7 @@ func (d *Manager) ComposeStrUp(name string, composeManifests []ComposeInlineMani
 		return utils.StrHash(mergedContent)
 	}).(pulumi.StringOutput)
 
-	// We include a hash of the manifest contents in the environment variables to trigger an update when a manifest changes
+	// We include a hash of the manifests content in the environment variables to trigger an update when a manifest changes
 	// This is a workaround to avoid a force replace with Triggers when the content of the manifest changes
 	envVars["CONTENT_HASH"] = contentHash
 

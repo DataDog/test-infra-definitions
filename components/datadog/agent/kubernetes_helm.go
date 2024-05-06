338857aaf04de28ef034a4cf8cda9020d3be262a
Vincent Boulineau <vincent.boulineau@datadoghq.com>
2024-05-06 19:54:28 +0200

    Move EKS Windows nodes to managed nodegroups

--- components/datadog/agent/kubernetes_helm.go
+++ components/datadog/agent/kubernetes_helm.go
@@ -53,7 +53,7 @@ type HelmComponent struct {
 func NewHelmInstallation(e config.Env, args HelmInstallationArgs, opts ...pulumi.ResourceOption) (*HelmComponent, error) {
 	apiKey := e.AgentAPIKey()
 	appKey := e.AgentAPPKey()
-	installName := "dda"
+	baseName := "dda"
 	opts = append(opts, pulumi.Providers(args.KubeProvider), e.WithProviders(config.ProviderRandom), pulumi.Parent(args.KubeProvider), pulumi.DeletedWith(args.KubeProvider))
 
 	helmComponent := &HelmComponent{}
@@ -89,7 +89,7 @@ func NewHelmInstallation(e config.Env, args HelmInstallationArgs, opts ...pulumi
 	secret, err := corev1.NewSecret(e.Ctx(), "datadog-credentials", &corev1.SecretArgs{
 		Metadata: metav1.ObjectMetaArgs{
 			Namespace: ns.Metadata.Name(),
-			Name:      pulumi.Sprintf("%s-datadog-credentials", installName),
+			Name:      pulumi.Sprintf("%s-datadog-credentials", baseName),
 		},
 		StringData: pulumi.StringMap{
 			"api-key": apiKey,
@@ -124,12 +124,8 @@ func NewHelmInstallation(e config.Env, args HelmInstallationArgs, opts ...pulumi
 	}
 	clusterAgentImagePath, clusterAgentImageTag := utils.ParseImageReference(clusterAgentImagePath)
 
-	linuxInstallName := installName
-	if args.DeployWindows {
-		linuxInstallName += "-linux"
-	}
-
-	values := buildLinuxHelmValues(installName, agentImagePath, agentImageTag, clusterAgentImagePath, clusterAgentImageTag, randomClusterAgentToken.Result, !args.DisableLogsContainerCollectAll)
+	linuxInstallName := baseName + "-linux"
+	values := buildLinuxHelmValues(baseName, agentImagePath, agentImageTag, clusterAgentImagePath, clusterAgentImageTag, randomClusterAgentToken.Result, !args.DisableLogsContainerCollectAll)
 	values.configureImagePullSecret(imgPullSecret)
 	values.configureFakeintake(e, args.Fakeintake)
 
@@ -154,14 +150,15 @@ func NewHelmInstallation(e config.Env, args HelmInstallationArgs, opts ...pulumi
 	}
 
 	if args.DeployWindows {
-		values := buildWindowsHelmValues(installName, agentImagePath, agentImageTag, clusterAgentImagePath, clusterAgentImageTag)
+		values := buildWindowsHelmValues(baseName, agentImagePath, agentImageTag, clusterAgentImagePath, clusterAgentImageTag)
 		values.configureImagePullSecret(imgPullSecret)
 		values.configureFakeintake(e, args.Fakeintake)
 
+		windowsInstallName := baseName + "-windows"
 		windows, err := helm.NewInstallation(e, helm.InstallArgs{
 			RepoURL:     DatadogHelmRepo,
 			ChartName:   "datadog",
-			InstallName: installName + "-windows",
+			InstallName: windowsInstallName,
 			Namespace:   args.Namespace,
 			ValuesYAML:  args.ValuesYAML,
 			Values:      pulumi.Map(values),
@@ -188,11 +185,11 @@ func NewHelmInstallation(e config.Env, args HelmInstallationArgs, opts ...pulumi
 
 type HelmValues pulumi.Map
 
-func buildLinuxHelmValues(installName, agentImagePath, agentImageTag, clusterAgentImagePath, clusterAgentImageTag string, clusterAgentToken pulumi.StringInput, logsContainerCollectAll bool) HelmValues {
+func buildLinuxHelmValues(baseName, agentImagePath, agentImageTag, clusterAgentImagePath, clusterAgentImageTag string, clusterAgentToken pulumi.StringInput, logsContainerCollectAll bool) HelmValues {
 	return HelmValues{
 		"datadog": pulumi.Map{
-			"apiKeyExistingSecret": pulumi.String(installName + "-datadog-credentials"),
-			"appKeyExistingSecret": pulumi.String(installName + "-datadog-credentials"),
+			"apiKeyExistingSecret": pulumi.String(baseName + "-datadog-credentials"),
+			"appKeyExistingSecret": pulumi.String(baseName + "-datadog-credentials"),
 			"checksCardinality":    pulumi.String("high"),
 			"logs": pulumi.Map{
 				"enabled":             pulumi.Bool(true),
@@ -391,12 +388,12 @@ func buildLinuxHelmValues(installName, agentImagePath, agentImageTag, clusterAge
 	}
 }
 
-func buildWindowsHelmValues(installName string, agentImagePath, agentImageTag, _, _ string) HelmValues {
+func buildWindowsHelmValues(baseName string, agentImagePath, agentImageTag, _, _ string) HelmValues {
 	return HelmValues{
 		"targetSystem": pulumi.String("windows"),
 		"datadog": pulumi.Map{
-			"apiKeyExistingSecret": pulumi.String(installName + "-datadog-credentials"),
-			"appKeyExistingSecret": pulumi.String(installName + "-datadog-credentials"),
+			"apiKeyExistingSecret": pulumi.String(baseName + "-datadog-credentials"),
+			"appKeyExistingSecret": pulumi.String(baseName + "-datadog-credentials"),
 			"checksCardinality":    pulumi.String("high"),
 			"logs": pulumi.Map{
 				"enabled":             pulumi.Bool(true),
@@ -430,8 +427,8 @@ func buildWindowsHelmValues(installName string, agentImagePath, agentImageTag, _
 		},
 		"existingClusterAgent": pulumi.Map{
 			"join":                 pulumi.Bool(true),
-			"serviceName":          pulumi.String(installName + "-linux-datadog-cluster-agent"),
-			"tokenSecretName":      pulumi.String(installName + "-linux-datadog-cluster-agent"),
+			"serviceName":          pulumi.String(baseName + "-linux-datadog-cluster-agent"),
+			"tokenSecretName":      pulumi.String(baseName + "-linux-datadog-cluster-agent"),
 			"clusterchecksEnabled": pulumi.Bool(false),
 		},
 		"clusterChecksRunner": pulumi.Map{
--- resources/aws/eks/nodeGroups.go
+++ resources/aws/eks/nodeGroups.go
@@ -1,15 +1,14 @@
 package eks
 
 import (
-	"encoding/base64"
-	"fmt"
+	"strings"
 
 	"github.com/DataDog/test-infra-definitions/common/config"
+	"github.com/DataDog/test-infra-definitions/common/utils"
 	"github.com/DataDog/test-infra-definitions/resources/aws"
 
 	awsEks "github.com/pulumi/pulumi-aws/sdk/v6/go/aws/eks"
 	awsIam "github.com/pulumi/pulumi-aws/sdk/v6/go/aws/iam"
-	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/ssm"
 	"github.com/pulumi/pulumi-eks/sdk/v2/go/eks"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
@@ -18,27 +17,37 @@ const (
 	amazonLinux2AMD64AmiType = "AL2_x86_64"
 	amazonLinux2ARM64AmiType = "AL2_ARM_64"
 	bottlerocketAmiType      = "BOTTLEROCKET_x86_64"
-
-	windowsInitUserData = `<powershell>
-[string]$EKSBootstrapScriptFile = "$env:ProgramFiles\Amazon\EKS\Start-EKSBootstrap.ps1"
-& $EKSBootstrapScriptFile -EKSClusterName %s -KubeletExtraArgs "--register-with-taints=node.kubernetes.io/os=windows:NoSchedule" 3>&1 4>&1 5>&1 6>&1
-</powershell>
-`
+	windowsAmiType           = "WINDOWS_CORE_2022_x86_64"
 )
 
-func NewLinuxNodeGroup(e aws.Environment, cluster *eks.Cluster, nodeRole *awsIam.Role) (*eks.ManagedNodeGroup, error) {
-	return newManagedNodeGroup(e, "linux", cluster, nodeRole, amazonLinux2AMD64AmiType, e.DefaultInstanceType())
+func NewLinuxNodeGroup(e aws.Environment, cluster *eks.Cluster, nodeRole *awsIam.Role, opts ...pulumi.ResourceOption) (*eks.ManagedNodeGroup, error) {
+	return newManagedNodeGroup(e, "linux", cluster, nodeRole, amazonLinux2AMD64AmiType, e.DefaultInstanceType(), opts...)
 }
 
-func NewLinuxARMNodeGroup(e aws.Environment, cluster *eks.Cluster, nodeRole *awsIam.Role) (*eks.ManagedNodeGroup, error) {
-	return newManagedNodeGroup(e, "linux-arm", cluster, nodeRole, amazonLinux2ARM64AmiType, e.DefaultARMInstanceType())
+func NewLinuxARMNodeGroup(e aws.Environment, cluster *eks.Cluster, nodeRole *awsIam.Role, opts ...pulumi.ResourceOption) (*eks.ManagedNodeGroup, error) {
+	return newManagedNodeGroup(e, "linux-arm", cluster, nodeRole, amazonLinux2ARM64AmiType, e.DefaultARMInstanceType(), opts...)
 }
 
-func NewBottlerocketNodeGroup(e aws.Environment, cluster *eks.Cluster, nodeRole *awsIam.Role) (*eks.ManagedNodeGroup, error) {
-	return newManagedNodeGroup(e, "bottlerocket", cluster, nodeRole, bottlerocketAmiType, e.DefaultInstanceType())
+func NewBottlerocketNodeGroup(e aws.Environment, cluster *eks.Cluster, nodeRole *awsIam.Role, opts ...pulumi.ResourceOption) (*eks.ManagedNodeGroup, error) {
+	return newManagedNodeGroup(e, "bottlerocket", cluster, nodeRole, bottlerocketAmiType, e.DefaultInstanceType(), opts...)
 }
 
-func newManagedNodeGroup(e aws.Environment, name string, cluster *eks.Cluster, nodeRole *awsIam.Role, amiType, instanceType string) (*eks.ManagedNodeGroup, error) {
+func NewWindowsNodeGroup(e aws.Environment, cluster *eks.Cluster, nodeRole *awsIam.Role, opts ...pulumi.ResourceOption) (*eks.ManagedNodeGroup, error) {
+	return newManagedNodeGroup(e, "windows", cluster, nodeRole, windowsAmiType, e.DefaultInstanceType(), opts...)
+}
+
+func newManagedNodeGroup(e aws.Environment, name string, cluster *eks.Cluster, nodeRole *awsIam.Role, amiType, instanceType string, opts ...pulumi.ResourceOption) (*eks.ManagedNodeGroup, error) {
+	taints := awsEks.NodeGroupTaintArray{}
+	if strings.Contains(amiType, "WINDOWS") {
+		taints = append(taints,
+			awsEks.NodeGroupTaintArgs{
+				Key:    pulumi.String("node.kubernetes.io/os"),
+				Value:  pulumi.String("windows"),
+				Effect: pulumi.String("NO_SCHEDULE"),
+			},
+		)
+	}
+
 	return eks.NewManagedNodeGroup(e.Ctx(), e.Namer.ResourceName(name), &eks.ManagedNodeGroupArgs{
 		AmiType:             pulumi.StringPtr(amiType),
 		Cluster:             cluster.Core,
@@ -56,46 +65,6 @@ func newManagedNodeGroup(e aws.Environment, name string, cluster *eks.Cluster, n
 			Ec2SshKey:              pulumi.String(e.DefaultKeyPairName()),
 			SourceSecurityGroupIds: pulumi.ToStringArray(e.EKSAllowedInboundSecurityGroups()),
 		},
-	}, e.WithProviders(config.ProviderAWS, config.ProviderEKS))
-}
-
-func NewWindowsUnmanagedNodeGroup(e aws.Environment, cluster *eks.Cluster, nodeRole *awsIam.Role) (*eks.NodeGroup, error) {
-	windowsAmi, err := ssm.LookupParameter(e.Ctx(), &ssm.LookupParameterArgs{
-		Name: fmt.Sprintf("/aws/service/ami-windows-latest/Windows_Server-2022-English-Core-EKS_Optimized-%s/image_id", e.KubernetesVersion()),
-	}, e.WithProvider(config.ProviderAWS))
-	if err != nil {
-		return nil, err
-	}
-
-	return newUnmanagedNodeGroup(e, "windows-ng", cluster, nodeRole, pulumi.String(windowsAmi.Value), pulumi.String(e.DefaultInstanceType()), getUserData(windowsInitUserData, cluster.EksCluster.Name()))
-}
-
-func newUnmanagedNodeGroup(e aws.Environment, name string, cluster *eks.Cluster, nodeRole *awsIam.Role, ami, instanceType, userData pulumi.StringInput) (*eks.NodeGroup, error) {
-	instanceProfile, err := awsIam.NewInstanceProfile(e.Ctx(), e.Namer.ResourceName(name), &awsIam.InstanceProfileArgs{
-		Name: e.CommonNamer().DisplayName(255, pulumi.String(name)),
-		Role: nodeRole.Name,
-	}, e.WithProviders(config.ProviderAWS))
-	if err != nil {
-		return nil, err
-	}
-
-	return eks.NewNodeGroup(e.Ctx(), e.Namer.ResourceName(name), &eks.NodeGroupArgs{
-		NodeUserDataOverride: userData,
-		Cluster:              cluster.Core,
-		DesiredCapacity:      pulumi.Int(1),
-		// Currently not working
-		// ExtraNodeSecurityGroups: extraSecurityGroups,
-		KeyName:                      pulumi.StringPtr(e.DefaultKeyPairName()),
-		AmiId:                        ami,
-		InstanceType:                 instanceType,
-		NodeRootVolumeSize:           pulumi.Int(80),
-		NodeAssociatePublicIpAddress: pulumi.BoolRef(false),
-		InstanceProfile:              instanceProfile,
-	}, e.WithProviders(config.ProviderAWS, config.ProviderEKS))
-}
-
-func getUserData(userData string, clusterName pulumi.StringInput) pulumi.StringInput {
-	return clusterName.ToStringOutput().ApplyT(func(name string) pulumi.StringInput {
-		return pulumi.String(base64.StdEncoding.EncodeToString([]byte(fmt.Sprintf(userData, name))))
-	}).(pulumi.StringInput)
+		Taints: taints,
+	}, utils.MergeOptions(opts, e.WithProviders(config.ProviderAWS, config.ProviderEKS))...)
 }

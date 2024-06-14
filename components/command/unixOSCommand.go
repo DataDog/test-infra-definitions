b1653772e90a4e9bfab1fd71aec1c445f2e2a6ee
Florent Clarret <florent.clarret@datadoghq.com>
2024-06-14 09:47:40 +0000

    Restart the Agent after the config files are replaced (#884)
    * Restart the Agent after the config files are replaced
    
    * Revert "Restart the Agent after the config files are replaced"
    
    This reverts commit b282e8261bfbfdbad6647669622c749704b88caa.
    
    * Add missing dependencies when moving the file in NewCopyFile

--- components/command/unixOSCommand.go
+++ components/command/unixOSCommand.go
@@ -88,7 +88,7 @@ func (fs unixOSCommand) NewCopyFile(runner *Runner, name string, localPath, remo
 		return nil, err
 	}
 
-	moveCommand, err := fs.moveRemoteFile(runner, name, tempRemotePath, remotePath, true, utils.PulumiDependsOn(tempCopyFile))
+	moveCommand, err := fs.moveRemoteFile(runner, name, tempRemotePath, remotePath, true, utils.MergeOptions(opts, utils.PulumiDependsOn(tempCopyFile))...)
 	if err != nil {
 		return nil, err
 	}
@@ -122,5 +122,5 @@ func (fs unixOSCommand) moveRemoteFile(runner *Runner, name string, source, dest
 	copyCommand := pulumi.Sprintf(`cp '%v' '%v'`, source, destination)
 	createCommand := pulumi.Sprintf(`bash -c 'if [ -f '%v' ]; then mv -f '%v' '%v'; fi; %v'`, destination, destination, backupPath, copyCommand)
 	deleteCommand := pulumi.Sprintf(`bash -c 'if [ -f '%v' ]; then mv -f '%v' '%v'; else rm -f '%v'; fi'`, backupPath, backupPath, destination, destination)
-	return copyRemoteFile(runner, fmt.Sprintf("move-file-%s", name), createCommand, deleteCommand, sudo, utils.MergeOptions(opts, pulumi.DeleteBeforeReplace(true))...)
+	return copyRemoteFile(runner, fmt.Sprintf("move-file-%s", name), createCommand, deleteCommand, sudo, utils.MergeOptions(opts, pulumi.ReplaceOnChanges([]string{"*"}), pulumi.DeleteBeforeReplace(true))...)
 }

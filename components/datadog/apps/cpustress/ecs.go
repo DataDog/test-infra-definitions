9e9168a64a390916b75e0aaa4ce822b59791c4b2
Kevin Fairise <kevin.fairise@datadoghq.com>
2024-06-26 13:50:21 +0200

    Create and use ecs Workload

--- components/datadog/apps/cpustress/ecs.go
+++ components/datadog/apps/cpustress/ecs.go
@@ -2,8 +2,8 @@ package cpustress
 
 import (
 	"github.com/DataDog/test-infra-definitions/common/config"
+	ecsComp "github.com/DataDog/test-infra-definitions/components/ecs"
 	"github.com/DataDog/test-infra-definitions/resources/aws"
-
 	"github.com/pulumi/pulumi-awsx/sdk/v2/go/awsx/awsx"
 	"github.com/pulumi/pulumi-awsx/sdk/v2/go/awsx/ecs"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
@@ -13,11 +13,11 @@ type EcsComponent struct {
 	pulumi.ResourceState
 }
 
-func EcsAppDefinition(e aws.Environment, clusterArn pulumi.StringInput, opts ...pulumi.ResourceOption) (*EcsComponent, error) {
+func EcsAppDefinition(e aws.Environment, clusterArn pulumi.StringInput, opts ...pulumi.ResourceOption) (*ecsComp.Workload, error) {
 	namer := e.Namer.WithPrefix("cpustress")
 	opts = append(opts, e.WithProviders(config.ProviderAWS, config.ProviderAWSX))
 
-	ecsComponent := &EcsComponent{}
+	ecsComponent := &ecsComp.Workload{}
 	if err := e.Ctx().RegisterComponentResource("dd:apps", namer.ResourceName("grp"), ecsComponent, opts...); err != nil {
 		return nil, err
 	}
--- components/datadog/apps/tracegen/ecs.go
+++ components/datadog/apps/tracegen/ecs.go
@@ -2,6 +2,7 @@ package tracegen
 
 import (
 	"github.com/DataDog/test-infra-definitions/common/config"
+	ecsComp "github.com/DataDog/test-infra-definitions/components/ecs"
 	"github.com/DataDog/test-infra-definitions/resources/aws"
 	classicECS "github.com/pulumi/pulumi-aws/sdk/v6/go/aws/ecs"
 	"github.com/pulumi/pulumi-awsx/sdk/v2/go/awsx/awsx"
@@ -13,11 +14,11 @@ type EcsComponent struct {
 	pulumi.ResourceState
 }
 
-func EcsAppDefinition(e aws.Environment, clusterArn pulumi.StringInput, opts ...pulumi.ResourceOption) (*EcsComponent, error) {
+func EcsAppDefinition(e aws.Environment, clusterArn pulumi.StringInput, opts ...pulumi.ResourceOption) (*ecsComp.Workload, error) {
 	namer := e.Namer.WithPrefix("tracegen")
 	opts = append(opts, e.WithProviders(config.ProviderAWS, config.ProviderAWSX))
 
-	ecsComponent := &EcsComponent{}
+	ecsComponent := &ecsComp.Workload{}
 	if err := e.Ctx().RegisterComponentResource("dd:apps", namer.ResourceName("grp"), ecsComponent, opts...); err != nil {
 		return nil, err
 	}

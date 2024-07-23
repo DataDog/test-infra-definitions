c574dd8c9d82d27607a9d65a35ce140ede896b5a
Branden Clark <branden.clark@datadoghq.com>
2024-07-23 12:37:46 -0400

    Add missing devmode option (#887)

--- tasks/setup.py
+++ tasks/setup.py
@@ -114,7 +114,7 @@ def _check_config(config: Config):
 
 def setupAWSConfig(config: Config):
     if config.configParams is None:
-        config.configParams = Config.Params(aws=None, agent=None, pulumi=None, azure=None)
+        config.configParams = Config.Params(aws=None, agent=None, pulumi=None, azure=None, devMode=False)
     if config.configParams.aws is None:
         config.configParams.aws = Config.Params.Aws(keyPairName=None, publicKeyPath=None, account=None, teamTag=None)
 

373c0905d2d26e98a84ccbaa15cf5d5af63142b5
Fanny Jiang <fanny.jiang@datadoghq.com>
2024-07-23 18:15:09 -0400

    Refactor helm kubernetes agent (#881)

---
+++ components/datadog/agent/helm/kubernetes_agent.go
@@ -0,0 +1,72 @@
+package helm
+
+import (
+	"github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes"
+
+	"github.com/DataDog/test-infra-definitions/components/datadog/agent"
+
+	"github.com/DataDog/test-infra-definitions/common/config"
+	"github.com/DataDog/test-infra-definitions/components"
+	"github.com/DataDog/test-infra-definitions/components/datadog/kubernetesagentparams"
+)
+
+func NewKubernetesAgent(e config.Env, resourceName string, kubeProvider *kubernetes.Provider, options ...kubernetesagentparams.Option) (*agent.KubernetesAgent, error) {
+	return components.NewComponent(e, resourceName, func(comp *agent.KubernetesAgent) error {
+		params, err := kubernetesagentparams.NewParams(e, options...)
+		if err != nil {
+			return err
+		}
+
+		helmComponent, err := agent.NewHelmInstallation(e, agent.HelmInstallationArgs{
+			KubeProvider:                   kubeProvider,
+			DeployWindows:                  params.DeployWindows,
+			Namespace:                      params.Namespace,
+			ValuesYAML:                     params.HelmValues,
+			Fakeintake:                     params.FakeIntake,
+			AgentFullImagePath:             params.AgentFullImagePath,
+			ClusterAgentFullImagePath:      params.ClusterAgentFullImagePath,
+			DisableLogsContainerCollectAll: params.DisableLogsContainerCollectAll,
+		}, params.PulumiResourceOptions...)
+		if err != nil {
+			return err
+		}
+
+		platform := "linux"
+		appVersion := helmComponent.LinuxHelmReleaseStatus.AppVersion().Elem()
+		version := helmComponent.LinuxHelmReleaseStatus.Version().Elem()
+
+		if params.DeployWindows {
+			platform = "windows"
+			appVersion = helmComponent.WindowsHelmReleaseStatus.AppVersion().Elem()
+			version = helmComponent.WindowsHelmReleaseStatus.Version().Elem()
+		}
+
+		baseName := "dda-" + platform
+
+		comp.NodeAgent, err = agent.NewKubernetesObjRef(e, baseName+"-nodeAgent", params.Namespace, "Pod", appVersion, version, map[string]string{
+			"app": baseName + "-datadog",
+		})
+
+		if err != nil {
+			return err
+		}
+
+		comp.ClusterAgent, err = agent.NewKubernetesObjRef(e, baseName+"-clusterAgent", params.Namespace, "Pod", appVersion, version, map[string]string{
+			"app": baseName + "-datadog-cluster-agent",
+		})
+
+		if err != nil {
+			return err
+		}
+
+		comp.ClusterChecks, err = agent.NewKubernetesObjRef(e, baseName+"-clusterChecks", params.Namespace, "Pod", appVersion, version, map[string]string{
+			"app": baseName + "-datadog-clusterchecks",
+		})
+
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+}
---
+++ components/datadog/agent/kubernetes_object_ref.go
@@ -0,0 +1,55 @@
+package agent
+
+import (
+	"encoding/json"
+
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+
+	"github.com/DataDog/test-infra-definitions/common/config"
+	"github.com/DataDog/test-infra-definitions/components"
+)
+
+type KubernetesObjRefOutput struct {
+	components.JSONImporter
+
+	Namespace      string `json:"namespace"`
+	Name           string `json:"name"`
+	Kind           string `json:"kind"`
+	AppVersion     string `json:"installAppVersion"`
+	Version        string `json:"installVersion"`
+	LabelSelectors string `json:"labelSelectors"`
+}
+
+type KubernetesObjectRef struct {
+	pulumi.ResourceState
+	components.Component
+
+	Namespace      pulumi.String       `pulumi:"namespace"`
+	Name           pulumi.String       `pulumi:"name"`
+	Kind           pulumi.String       `pulumi:"kind"`
+	AppVersion     pulumi.StringOutput `pulumi:"installAppVersion"`
+	Version        pulumi.StringOutput `pulumi:"installVersion"`
+	LabelSelectors pulumi.String       `pulumi:"labelSelectors"`
+}
+
+func NewKubernetesObjRef(e config.Env, name string, namespace string, kind string, appVersion pulumi.StringOutput, version pulumi.StringOutput, labelSelectors map[string]string) (*KubernetesObjectRef, error) {
+	return components.NewComponent(e, name, func(comp *KubernetesObjectRef) error {
+		comp.Name = pulumi.String(name)
+		comp.Namespace = pulumi.String(namespace)
+		comp.Kind = pulumi.String(kind)
+		comp.AppVersion = appVersion
+		comp.Version = version
+
+		labelSelectorsJSONStr, err := json.Marshal(labelSelectors)
+		if err != nil {
+			return err
+		}
+		comp.LabelSelectors = pulumi.String(labelSelectorsJSONStr)
+
+		return nil
+	})
+}
+
+func (h *KubernetesObjectRef) Export(ctx *pulumi.Context, out *KubernetesObjRefOutput) error {
+	return components.Export(ctx, h, out)
+}

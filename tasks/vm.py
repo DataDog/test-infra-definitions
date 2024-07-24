d753f4d2940925a70183bd95b5d184acf91d1cff
Branden Clark <branden.clark@datadoghq.com>
2024-07-24 14:05:03 -0400

    Add RDP helper for VMs (#885)

--- tasks/vm.py
+++ tasks/vm.py
@@ -1,9 +1,14 @@
 from typing import Optional
 
+import pyperclip
 from invoke.context import Context
+from invoke.exceptions import Exit
 from invoke.tasks import task
+from pydantic import ValidationError
 
-from . import doc
+from tasks.config import Config, get_full_profile_path
+
+from . import config, doc, tool
 
 
 @task(
@@ -71,6 +76,131 @@ def create_vm(
         no_verify,
         ssh_user,
     )
+
+
+def _filter_aws_resource(resource, instance_id: Optional[str] = None, ip: Optional[str] = None):
+    if instance_id and resource["id"] != instance_id:
+        return None
+    if ip and resource["outputs"]["privateIp"] != ip:
+        return None
+    return resource
+
+
+def _get_windows_password(
+    ctx: Context,
+    cfg: Config,
+    full_stack_name: str,
+    use_aws_vault: Optional[bool] = True,
+    instance_id: Optional[str] = None,
+    ip: Optional[str] = None,
+):
+    resources = tool.get_stack_json_resources(ctx, full_stack_name)
+    vms = []
+    for r in resources:
+        if not r["type"].startswith("aws:ec2/instance:Instance"):
+            continue
+        vms.append(r)
+    if not vms:
+        raise Exit("No VM found in the stack.")
+
+    out = []
+    for r in vms:
+        if not _filter_aws_resource(r, instance_id, ip):
+            continue
+        vm_id = r["id"]
+        aws_account = cfg.get_aws().get_account()
+        # TODO: could xref with r['inputs']['keyName']
+        key_path = cfg.get_aws().privateKeyPath
+        if not key_path:
+            raise Exit("No privateKeyPath found in the config.")
+        password = tool.get_aws_instance_password_data(
+            ctx, vm_id, key_path, aws_account=aws_account, use_aws_vault=use_aws_vault
+        )
+        if password:
+            out.append({"vm_id": vm_id, "resource": r, "password": password})
+    return out
+
+
+@task(
+    help={
+        "config_path": doc.config_path,
+        "stack_name": "Name of stack that contains hosts to RDP into",
+        "use_aws_vault": doc.use_aws_vault,
+        "ip": "Filter to VM with this IP address",
+        "instance_id": "Filter to VM with this id",
+    }
+)
+def get_vm_password(
+    ctx: Context,
+    stack_name: Optional[str] = None,
+    config_path: Optional[str] = None,
+    ip: Optional[str] = None,
+    instance_id: Optional[str] = None,
+    use_aws_vault: Optional[bool] = True,
+):
+    """
+    Get the password of a new virtual machine in a stack.
+    """
+    try:
+        cfg = config.get_local_config(config_path)
+    except ValidationError as e:
+        raise Exit(f"Error in config {get_full_profile_path(config_path)}:{e}")
+
+    if not stack_name:
+        raise Exit("Please provide a stack name to connect to.")
+
+    out = _get_windows_password(ctx, cfg, stack_name, use_aws_vault=use_aws_vault, instance_id=instance_id, ip=ip)
+    if not out:
+        raise Exit(
+            "No VM found in the stack, or no password available. Verify that keyPairName and publicKeyPath are an RSA key. run `inv setup.debug` for automated help."
+        )
+    for vm in out:
+        vm_id = vm["vm_id"]
+        vm_ip = vm["resource"]["outputs"]["privateIp"]
+        password = vm["password"]
+        print(f"Password for VM {vm_id} ({vm_ip}): {password}")
+
+
+@task(
+    help={
+        "config_path": doc.config_path,
+        "stack_name": "Name of stack that contains hosts to RDP into",
+        "use_aws_vault": doc.use_aws_vault,
+        "ip": "Filter to VM with this IP address",
+        "instance_id": "Filter to VM with this id",
+    }
+)
+def rdp_vm(
+    ctx: Context,
+    stack_name: Optional[str] = None,
+    config_path: Optional[str] = None,
+    ip: Optional[str] = None,
+    instance_id: Optional[str] = None,
+    use_aws_vault: Optional[bool] = True,
+):
+    """
+    Open an RDP connection to a new virtual machine in a stack.
+    """
+    try:
+        cfg = config.get_local_config(config_path)
+    except ValidationError as e:
+        raise Exit(f"Error in config {get_full_profile_path(config_path)}:{e}")
+
+    if not stack_name:
+        raise Exit("Please provide a stack name to connect to.")
+
+    out = _get_windows_password(ctx, cfg, stack_name, use_aws_vault=use_aws_vault, instance_id=instance_id, ip=ip)
+    if not out:
+        raise Exit(
+            "No VM found in the stack, or no password available. Verify that keyPairName and publicKeyPath are an RSA key. run `inv setup.debug` for automated help."
+        )
+    for vm in out:
+        vm_ip = vm["resource"]["outputs"]["privateIp"]
+        password = vm["password"]
+        tool.rdp(ctx, vm_ip)
+        print(f"Password for VM {vm['vm_id']} ({vm_ip}): {password}")
+        print("Username is Administrator, password has been copied to clipboard")
+        pyperclip.copy(password)
 
 
 @task(

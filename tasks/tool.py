a44834ce59afa324a888ddec9d25e174841d5f28
Branden Clark <branden.clark@datadoghq.com>
2024-08-22 11:40:26 -0400

    Add AWS key create/import to setup task (#1045)

--- tasks/tool.py
+++ tasks/tool.py
@@ -33,6 +33,19 @@ def ask(question: str) -> str:
     return input(colored(question, "blue"))
 
 
+def ask_yesno(question: str, default='N') -> bool:
+    res = ""
+    yes_opts = ["y", "yes"]
+    no_opts = ["n", "no"]
+    while res.lower() not in (yes_opts + no_opts):
+        res = ask(question + f" [Y/N] Default [{default}]: ")
+        if res == "":
+            res = default
+            break
+
+    return res.lower() in yes_opts
+
+
 def debug(msg: str):
     print(colored(msg, "white"))
 
@@ -98,6 +111,22 @@ def get_aws_wrapper(
     return f"aws-vault exec sso-{aws_account}-account-admin -- "
 
 
+def get_aws_cmd(
+    cmd: str,
+    use_aws_vault: Optional[bool] = True,
+    aws_account: Optional[str] = None,
+) -> str:
+    wrapper = ""
+    if use_aws_vault:
+        if aws_account is None:
+            raise Exit("AWS account is required when using aws-vault.")
+        wrapper = get_aws_wrapper(aws_account)
+    # specify .exe for windows to work around conflicts with aws.rb
+    aws = "aws.exe" if is_windows() else "aws"
+    cmd = f"{wrapper}{aws} {cmd}"
+    return cmd
+
+
 def is_linux():
     return platform.system() == "Linux"
 
@@ -111,7 +140,7 @@ def get_aws_instance_password_data(
 ) -> str:
     buffer = StringIO()
     with ctx.cd(_get_root_path()):
-        cmd = f"aws ec2 get-password-data --instance-id {vm_id} --priv-launch-key {key_path}"
+        cmd = f'aws ec2 get-password-data --instance-id "{vm_id}" --priv-launch-key "{key_path}"'
         if use_aws_vault:
             if aws_account is None:
                 raise Exit("AWS account is required when using aws-vault.")

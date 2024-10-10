1d0d83f1bf2a42b4d7d6637bb4a91090835c4e30
pducolin <paola.ducolin@datadoghq.com>
2024-10-10 11:40:24 +0200

    [azure] allow resetting keys in ssh setup script

--- resources/azure/compute/setup-ssh.ps1
+++ resources/azure/compute/setup-ssh.ps1
@@ -4,11 +4,24 @@ param(
 )
 
 $service = Get-Service -Name sshd -ErrorAction SilentlyContinue
+
 if ($service -ne $null) {
+  Write-Host "Stop sshd service"
   Stop-Service sshd
+} 
+
+if (-not (Get-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0  | Where-Object { $_.State -eq 'Installed' })) {
+  Write-Host "Add OpenSSH Server capability"
+  Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+} else {
+  Write-Host "OpenSSH Server capability already installed"
 }
-Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 Set-Service -Name sshd -StartupType Automatic
+if (Test-Path $env:ProgramData\ssh\administrators_authorized_keys) { 
+  Write-Host "Remove existing administrators_authorized_keys file"
+  Remove-Item $env:ProgramData\ssh\administrators_authorized_keys
+}
+New-Item -Path $env:ProgramData\ssh -Name administrators_authorized_keys -ItemType file
 Add-Content -Path $env:ProgramData\ssh\administrators_authorized_keys -Value $authorizedKey
 icacls.exe ""$env:ProgramData\ssh\administrators_authorized_keys"" /inheritance:r /grant ""Administrators:F"" /grant ""SYSTEM:F""
 New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force

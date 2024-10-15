bca6a8de5028ad4a9a16487c29407eb741bb7a21
Kevin Fairise <kevin.fairise@datadoghq.com>
2024-10-15 13:54:04 +0200

    Fix GKE authentication in CI

--- resources/gcp/environment.go
+++ resources/gcp/environment.go
@@ -77,6 +77,14 @@ func logIn(ctx *pulumi.Context) {
 		}
 	}
 
+	if os.Getenv("GOOGLE_CREDENTIALS") != "" {
+		fmt.Println("GOOGLE_CREDENTIALS environment detected, activating service account authentication")
+		cmd := exec.Command("gcloud", "auth", "activate-service-account", "--key-file", os.Getenv("GOOGLE_CREDENTIALS"))
+		if err := cmd.Run(); err != nil {
+			ctx.Log.Error(fmt.Sprintf("Error running `gcloud auth activate-service-account --key-file $GOOGLE_CREDENTIALS`: %v %v", err, cmd.Stderr), nil)
+		}
+	}
+
 	if envVariablesSet {
 		return
 	}
@@ -91,6 +99,19 @@ func logIn(ctx *pulumi.Context) {
 
 		if err != nil {
 			ctx.Log.Error(fmt.Sprintf("Error running `gcloud auth application-default login`: %v", err), nil)
+		}
+	}
+
+	cmd = exec.Command("gcloud", "auth", "print-access-token")
+
+	// There's no error if the token exists and is still valid
+	if err := cmd.Run(); err != nil {
+		// Login if the token is not valid anymore
+		cmd = exec.Command("gcloud", "auth", "login")
+		_, err := cmd.Output()
+
+		if err != nil {
+			ctx.Log.Error(fmt.Sprintf("Error running `gcloud auth login`: %v", err), nil)
 		}
 	}
 }

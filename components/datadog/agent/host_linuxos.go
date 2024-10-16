3806d2f3da26864f4e94b305552e419045ba0505
Florent Clarret <florent.clarret@datadoghq.com>
2024-10-16 11:58:09 +0200

    Add the possibility to run tests with an Agent 6 artifact

--- components/datadog/agent/host_linuxos.go
+++ components/datadog/agent/host_linuxos.go
@@ -2,16 +2,17 @@ package agent
 
 import (
 	"fmt"
-	"strings"
-
 	"github.com/DataDog/test-infra-definitions/components/command"
 	"github.com/DataDog/test-infra-definitions/components/datadog/agentparams"
 	"github.com/DataDog/test-infra-definitions/components/os"
 	remoteComp "github.com/DataDog/test-infra-definitions/components/remote"
+	"strings"
 
 	"github.com/pulumi/pulumi-command/sdk/go/command/remote"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
+
+const DefaultMajorVersion = "7"
 
 type agentLinuxManager struct {
 	targetOS os.OS
@@ -22,37 +23,43 @@ func newLinuxManager(host *remoteComp.Host) agentOSManager {
 }
 
 func (am *agentLinuxManager) getInstallCommand(version agentparams.PackageVersion, _ []string) (string, error) {
+	var commandLine string
+	var majorVersion string
+	testEnvVars := []string{}
+
+	if version.Major != "" {
+		majorVersion = version.Major
+	} else {
+		majorVersion = DefaultMajorVersion
+	}
+
 	if version.PipelineID != "" {
-		testEnvVars := []string{}
 		testEnvVars = append(testEnvVars, "TESTING_APT_URL=apttesting.datad0g.com")
 		// apt testing repo
 		// TESTING_APT_REPO_VERSION="pipeline-xxxxx-a7 7"
-		testEnvVars = append(testEnvVars, fmt.Sprintf(`TESTING_APT_REPO_VERSION="pipeline-%v-a7-%s 7"`, version.PipelineID, am.targetOS.Descriptor().Architecture))
+		testEnvVars = append(testEnvVars, fmt.Sprintf(`TESTING_APT_REPO_VERSION="pipeline-%v-a%v-%s %v"`, version.PipelineID, majorVersion, am.targetOS.Descriptor().Architecture, majorVersion))
 		testEnvVars = append(testEnvVars, "TESTING_YUM_URL=yumtesting.datad0g.com")
 		// yum testing repo
 		// TESTING_YUM_VERSION_PATH="testing/pipeline-xxxxx-a7/7"
-		testEnvVars = append(testEnvVars, fmt.Sprintf("TESTING_YUM_VERSION_PATH=testing/pipeline-%v-a7/7", version.PipelineID))
-		commandLine := strings.Join(testEnvVars, " ")
+		testEnvVars = append(testEnvVars, fmt.Sprintf("TESTING_YUM_VERSION_PATH=testing/pipeline-%v-a%v/%v", version.PipelineID, majorVersion, majorVersion))
+	} else {
+		testEnvVars = append(testEnvVars, fmt.Sprintf("DD_AGENT_MAJOR_VERSION=%v", majorVersion))
 
-		return fmt.Sprintf(
-			`for i in 1 2 3 4 5; do curl -fsSL https://s3.amazonaws.com/dd-agent/scripts/%v -o install-script.sh && break || sleep $((2**$i)); done &&  for i in 1 2 3; do DD_API_KEY=%%s %v DD_INSTALL_ONLY=true bash install-script.sh  && exit 0  || sleep $((2**$i)); done; exit 1`,
-			"install_script_agent7.sh",
-			commandLine), nil
+		if version.Minor != "" {
+			testEnvVars = append(testEnvVars, fmt.Sprintf("DD_AGENT_MINOR_VERSION=%v", version.Minor))
+		}
+
+		if version.Channel != "" && version.Channel != agentparams.StableChannel {
+			testEnvVars = append(testEnvVars, "REPO_URL=datad0g.com")
+			testEnvVars = append(testEnvVars, fmt.Sprintf("DD_AGENT_DIST_CHANNEL=%s", version.Channel))
+		}
 	}
 
-	commandLine := fmt.Sprintf("DD_AGENT_MAJOR_VERSION=%v ", version.Major)
-
-	if version.Minor != "" {
-		commandLine += fmt.Sprintf("DD_AGENT_MINOR_VERSION=%v ", version.Minor)
-	}
-
-	if version.Channel != "" && version.Channel != agentparams.StableChannel {
-		commandLine += fmt.Sprintf("REPO_URL=datad0g.com DD_AGENT_DIST_CHANNEL=%s ", version.Channel)
-	}
+	commandLine = strings.Join(testEnvVars, " ")
 
 	return fmt.Sprintf(
 		`for i in 1 2 3 4 5; do curl -fsSL https://s3.amazonaws.com/dd-agent/scripts/%v -o install-script.sh && break || sleep $((2**$i)); done &&  for i in 1 2 3; do DD_API_KEY=%%s %v DD_INSTALL_ONLY=true bash install-script.sh  && exit 0 || sleep $((2**$i)); done; exit 1`,
-		fmt.Sprintf("install_script_agent%s.sh", version.Major),
+		fmt.Sprintf("install_script_agent%s.sh", majorVersion),
 		commandLine), nil
 }
 

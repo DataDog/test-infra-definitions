b8c972aa9f6dcf2004092f9b4c89a5ecfe9c0e82
Ethan Wood-Thomas <ethan.woodthomas@datadoghq.com>
2024-10-18 11:15:37 -0400

    [CONTINT-4460] fix ecs fargate bug to use env specified agent image (#1200)

--- components/datadog/agent/ecsFargate.go
+++ components/datadog/agent/ecsFargate.go
@@ -11,9 +11,8 @@ import (
 
 func ECSFargateLinuxContainerDefinition(e config.Env, image string, apiKeySSMParamName pulumi.StringInput, fakeintake *fakeintake.Fakeintake, logConfig ecs.TaskDefinitionLogConfigurationPtrInput) *ecs.TaskDefinitionContainerDefinitionArgs {
 	if image == "" {
-		image = dockerAgentFullImagePath(e, "public.ecr.aws/datadog/agent", "latest", false)
+		image = dockerAgentFullImagePath(e, "public.ecr.aws/datadog/agent", "", false)
 	}
-
 	return &ecs.TaskDefinitionContainerDefinitionArgs{
 		Cpu:       pulumi.IntPtr(0),
 		Name:      pulumi.String("datadog-agent"),
@@ -97,9 +96,8 @@ func ECSFargateLinuxContainerDefinition(e config.Env, image string, apiKeySSMPar
 // https://docs.aws.amazon.com/AmazonECS/latest/developerguide/tutorial-deploy-fluentbit-on-windows.html
 func ECSFargateWindowsContainerDefinition(e config.Env, image string, apiKeySSMParamName pulumi.StringInput, fakeintake *fakeintake.Fakeintake) *ecs.TaskDefinitionContainerDefinitionArgs {
 	if image == "" {
-		image = dockerAgentFullImagePath(e, "public.ecr.aws/datadog/agent", "latest", false)
+		image = dockerAgentFullImagePath(e, "public.ecr.aws/datadog/agent", "", false)
 	}
-
 	return &ecs.TaskDefinitionContainerDefinitionArgs{
 		Cpu:       pulumi.IntPtr(0),
 		Name:      pulumi.String("datadog-agent"),

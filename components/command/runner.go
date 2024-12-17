2448f9d546cfc55b2b49e3c2e75cc90b3c64e24c
Celian Raimbault <celian.raimbault@datadoghq.com>
2024-12-17 15:22:41 +0100

    [refactor-runner-adxt-783] command: Fixed env + refactored podman vm.go

--- components/command/runner.go
+++ components/command/runner.go
@@ -22,27 +22,42 @@ type Args struct {
 	Environment              pulumi.StringMap
 	RequirePasswordFromStdin bool
 	Sudo                     bool
+	// Only used for local commands
+	LocalAssetPaths pulumi.StringArrayInput
+	LocalDir        pulumi.StringInput
 }
 
-func (args *Args) toLocalCommandArgs(config RunnerConfiguration, osCommand OSCommand) *local.CommandArgs {
+func (args *Args) toLocalCommandArgs(config RunnerConfiguration, osCommand OSCommand) (*local.CommandArgs, error) {
 	return &local.CommandArgs{
-		Create:   osCommand.BuildCommandString(args.Create, args.Environment, args.Sudo, args.RequirePasswordFromStdin, config.user),
-		Update:   osCommand.BuildCommandString(args.Update, args.Environment, args.Sudo, args.RequirePasswordFromStdin, config.user),
-		Delete:   osCommand.BuildCommandString(args.Delete, args.Environment, args.Sudo, args.RequirePasswordFromStdin, config.user),
-		Triggers: args.Triggers,
-		Stdin:    args.Stdin,
-	}
+		Create:      osCommand.BuildCommandString(args.Create, args.Environment, args.Sudo, args.RequirePasswordFromStdin, config.user),
+		Update:      osCommand.BuildCommandString(args.Update, args.Environment, args.Sudo, args.RequirePasswordFromStdin, config.user),
+		Delete:      osCommand.BuildCommandString(args.Delete, args.Environment, args.Sudo, args.RequirePasswordFromStdin, config.user),
+		Environment: args.Environment,
+		Triggers:    args.Triggers,
+		Stdin:       args.Stdin,
+		AssetPaths:  args.LocalAssetPaths,
+		Dir:         args.LocalDir,
+	}, nil
 }
 
-func (args *Args) toRemoteCommandArgs(config RunnerConfiguration, osCommand OSCommand) *remote.CommandArgs {
-	return &remote.CommandArgs{
-		Connection: config.connection,
-		Create:     osCommand.BuildCommandString(args.Create, args.Environment, args.Sudo, args.RequirePasswordFromStdin, config.user),
-		Update:     osCommand.BuildCommandString(args.Update, args.Environment, args.Sudo, args.RequirePasswordFromStdin, config.user),
-		Delete:     osCommand.BuildCommandString(args.Delete, args.Environment, args.Sudo, args.RequirePasswordFromStdin, config.user),
-		Triggers:   args.Triggers,
-		Stdin:      args.Stdin,
+func (args *Args) toRemoteCommandArgs(config RunnerConfiguration, osCommand OSCommand) (*remote.CommandArgs, error) {
+	// Ensure no local arguments are passed to remote commands
+	if args.LocalAssetPaths != nil {
+		return nil, fmt.Errorf("local asset paths are not supported in remote commands")
 	}
+	if args.LocalDir != nil {
+		return nil, fmt.Errorf("local dir is not supported in remote commands")
+	}
+
+	return &remote.CommandArgs{
+		Connection:  config.connection,
+		Create:      osCommand.BuildCommandString(args.Create, args.Environment, args.Sudo, args.RequirePasswordFromStdin, config.user),
+		Update:      osCommand.BuildCommandString(args.Update, args.Environment, args.Sudo, args.RequirePasswordFromStdin, config.user),
+		Delete:      osCommand.BuildCommandString(args.Delete, args.Environment, args.Sudo, args.RequirePasswordFromStdin, config.user),
+		Environment: args.Environment,
+		Triggers:    args.Triggers,
+		Stdin:       args.Stdin,
+	}, nil
 }
 
 // Transformer is a function that can be used to modify the command name and args.
@@ -173,7 +188,12 @@ func (r *RemoteRunner) Command(name string, args *Args, opts ...pulumi.ResourceO
 		r.e.Ctx().Log.Info(fmt.Sprintf("warning: running sudo command on a runner with user %s, discarding user", r.config.user), nil)
 	}
 
-	cmd, err := remote.NewCommand(r.e.Ctx(), r.namer.ResourceName("cmd", name), args.toRemoteCommandArgs(r.config, r.osCommand), utils.MergeOptions(r.options, opts...)...)
+	remoteArgs, err := args.toRemoteCommandArgs(r.config, r.osCommand)
+	if err != nil {
+		return nil, err
+	}
+
+	cmd, err := remote.NewCommand(r.e.Ctx(), r.namer.ResourceName("cmd", name), remoteArgs, utils.MergeOptions(r.options, opts...)...)
 
 	if err != nil {
 		return nil, err
@@ -233,7 +253,12 @@ func (r *LocalRunner) OsCommand() OSCommand {
 
 func (r *LocalRunner) Command(name string, args *Args, opts ...pulumi.ResourceOption) (Command, error) {
 	opts = utils.MergeOptions[pulumi.ResourceOption](opts, r.e.WithProviders(config.ProviderCommand))
-	cmd, err := local.NewCommand(r.e.Ctx(), r.namer.ResourceName("cmd", name), args.toLocalCommandArgs(r.config, r.osCommand), opts...)
+	localArgs, err := args.toLocalCommandArgs(r.config, r.osCommand)
+	if err != nil {
+		return nil, err
+	}
+
+	cmd, err := local.NewCommand(r.e.Ctx(), r.namer.ResourceName("cmd", name), localArgs, opts...)
 
 	if err != nil {
 		return nil, err
--- resources/local/podman/vm.go
+++ resources/local/podman/vm.go
@@ -6,11 +6,10 @@ import (
 	"path"
 	"runtime"
 
-	"github.com/DataDog/test-infra-definitions/common/config"
 	"github.com/DataDog/test-infra-definitions/common/utils"
+	"github.com/DataDog/test-infra-definitions/components/command"
 	resourceslocal "github.com/DataDog/test-infra-definitions/resources/local"
 
-	"github.com/pulumi/pulumi-command/sdk/go/command/local"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
@@ -23,22 +22,14 @@ var dockerfileContent string
 var customDockerConfig = "{}"
 
 func NewInstance(e resourceslocal.Environment, args VMArgs, opts ...pulumi.ResourceOption) (address pulumi.StringOutput, user string, port int, err error) {
-	// TODO A: Unix command / windows
-	// runner := command.NewLocalRunner(&e, command.LocalRunnerArgs{OSCommand: command.NewUnixOSCommand()})
-	// fileManager := command.NewFileManager(runner)
-
-	// runner.CopyUnixFile("copy-hey-ho", pulumi.String("/tmp/hey"), pulumi.String("/tmp/ho"))
-
-	// runner.Command("hey-ho", &command.Args{
-	// 	Create: pulumi.String("cp /tmp/hey /tmp/ho"),
-	// 	Delete: pulumi.String("rm /tmp/ho"),
-	// })
-	// TODO END
-
-	interpreter := []string{"/bin/bash", "-c"}
+	var osCommand command.OSCommand
 	if runtime.GOOS == "windows" {
-		interpreter = []string{"powershell", "-Command"}
+		osCommand = command.NewWindowsOSCommand()
+	} else {
+		osCommand = command.NewUnixOSCommand()
 	}
+	runner := command.NewLocalRunner(&e, command.LocalRunnerArgs{OSCommand: osCommand})
+	fileManager := command.NewFileManager(runner)
 
 	publicKey, err := os.ReadFile(e.DefaultPublicKeyPath())
 	if err != nil {
@@ -52,53 +43,48 @@ func NewInstance(e resourceslocal.Environment, args VMArgs, opts ...pulumi.Resou
 	dataPath := path.Join(homeDir, ".localpodman")
 	// TODO clean up the folder on stack destroy
 	// Requires a Runner refactor to reuse crossplatform commands
-	err = os.MkdirAll(dataPath, 0700)
-	// _, err = fileManager.CreateDirectory(dataPath, false)
+	dataDir, err := fileManager.CreateDirectory(dataPath, false)
 	if err != nil {
 		return pulumi.StringOutput{}, "", -1, err
 	}
-	println("DIR")
+
 	dockerfilePath := path.Join(dataPath, "Dockerfile")
-	// _, err = fileManager.CopyInlineFile(pulumi.String(dockerfileContent), dockerfilePath)
-	err = os.WriteFile(dockerfilePath, []byte(dockerfileContent), 0600)
+	dockerFile, err := fileManager.CopyInlineFile(pulumi.String(dockerfileContent), dockerfilePath, pulumi.DependsOn([]pulumi.Resource{dataDir}))
 	if err != nil {
 		return pulumi.StringOutput{}, "", -1, err
 	}
-	println("COPY")
 
 	// Use a config to avoid docker hooks that can call vault or other services (credHelpers)
-	err = os.WriteFile(path.Join(dataPath, "config.json"), []byte(customDockerConfig), 0600)
+	dockerConfig, err := fileManager.CopyInlineFile(pulumi.String(customDockerConfig), path.Join(dataPath, "config.json"), pulumi.DependsOn([]pulumi.Resource{dataDir}))
 	if err != nil {
 		return pulumi.StringOutput{}, "", -1, err
 	}
 
 	podmanCommand := "podman --config " + dataPath
 
-	opts = utils.MergeOptions(opts, e.WithProviders(config.ProviderCommand))
-	// TODO use NewLocalRunner
-	// requires a refactor to pass interpreter
-	buildPodman, err := local.NewCommand(e.Ctx(), e.CommonNamer().ResourceName("podman-build", args.Name), &local.CommandArgs{
-		Interpreter: pulumi.ToStringArray(interpreter),
-		Environment: pulumi.StringMap{"DOCKER_HOST_SSH_PUBLIC_KEY": pulumi.String(string(publicKey))},
-		Create:      pulumi.Sprintf("%s build --format=docker --build-arg DOCKER_HOST_SSH_PUBLIC_KEY=\"$DOCKER_HOST_SSH_PUBLIC_KEY\" -t %s .", podmanCommand, args.Name),
-		Delete:      pulumi.Sprintf("%s rmi %s", podmanCommand, args.Name),
-		Triggers:    pulumi.Array{},
-		AssetPaths:  pulumi.StringArray{},
-		Dir:         pulumi.String(dataPath),
+	opts = utils.MergeOptions(opts, pulumi.DependsOn([]pulumi.Resource{dockerFile, dockerConfig}))
+	buildPodman, err := runner.Command("podman-build"+args.Name, &command.Args{
+		Environment:     pulumi.StringMap{"DOCKER_HOST_SSH_PUBLIC_KEY": pulumi.String(string(publicKey))},
+		Create:          pulumi.Sprintf("%s build --format=docker --build-arg DOCKER_HOST_SSH_PUBLIC_KEY=\"$DOCKER_HOST_SSH_PUBLIC_KEY\" -t %s .", podmanCommand, args.Name),
+		Delete:          pulumi.Sprintf("%s rmi %s", podmanCommand, args.Name),
+		Triggers:        pulumi.Array{},
+		LocalAssetPaths: pulumi.StringArray{},
+		LocalDir:        pulumi.String(dataPath),
 	}, opts...)
+
 	if err != nil {
 		return pulumi.StringOutput{}, "", -1, err
 	}
 	opts = utils.MergeOptions(opts, pulumi.DependsOn([]pulumi.Resource{buildPodman}))
-	runPodman, err := local.NewCommand(e.Ctx(), e.CommonNamer().ResourceName("podman-run", args.Name), &local.CommandArgs{
-		Interpreter: pulumi.ToStringArray(interpreter),
-		Environment: pulumi.StringMap{"DOCKER_HOST_SSH_PUBLIC_KEY": pulumi.String(string(publicKey))},
-		Create:      pulumi.Sprintf("%s run -d --name=%[2]s_run -p 50022:22 %[2]s", podmanCommand, args.Name),
-		Delete:      pulumi.Sprintf("%s stop %[2]s_run && podman rm %[2]s_run", podmanCommand, args.Name),
-		Triggers:    pulumi.Array{},
-		AssetPaths:  pulumi.StringArray{},
-		Dir:         pulumi.String(dataPath),
+	runPodman, err := runner.Command("podman-run"+args.Name, &command.Args{
+		Environment:     pulumi.StringMap{"DOCKER_HOST_SSH_PUBLIC_KEY": pulumi.String(string(publicKey))},
+		Create:          pulumi.Sprintf("%s run -d --name=%[2]s_run -p 50022:22 %[2]s", podmanCommand, args.Name),
+		Delete:          pulumi.Sprintf("%s stop %[2]s_run && podman rm %[2]s_run", podmanCommand, args.Name),
+		Triggers:        pulumi.Array{},
+		LocalAssetPaths: pulumi.StringArray{},
+		LocalDir:        pulumi.String(dataPath),
 	}, opts...)
+
 	if err != nil {
 		return pulumi.StringOutput{}, "", -1, err
 	}
@@ -106,7 +92,7 @@ func NewInstance(e resourceslocal.Environment, args VMArgs, opts ...pulumi.Resou
 	e.Ctx().Log.Info("Running with container of type ubuntu", nil)
 
 	// hack to wait for the container to be up
-	ipAddress := runPodman.Stdout.ApplyT(func(_ string) string {
+	ipAddress := runPodman.StdoutOutput().ApplyT(func(_ string) string {
 		return "localhost"
 	}).(pulumi.StringOutput)
 

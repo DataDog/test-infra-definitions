a508d11b9183938a69d5a8bef1e1bd9880900723
Celian Raimbault <celian.raimbault@datadoghq.com>
2025-01-08 17:29:54 +0100

    [e2e-lb3-adxt-769] fakeintake: Export scheme

--- components/datadog/fakeintake/component.go
+++ components/datadog/fakeintake/component.go
@@ -9,7 +9,8 @@ import (
 type FakeintakeOutput struct { // nolint:revive, We want to keep the name as <Component>Output
 	components.JSONImporter
 
-	URL string `json:"url"`
+	Scheme string `json:"scheme"`
+	URL    string `json:"url"`
 }
 
 type Fakeintake struct {
@@ -19,7 +20,7 @@ type Fakeintake struct {
 	// It's cleaner to export the full URL, but the Agent requires only host in some cases.
 	// Keeping those internal to Pulumi program.
 	Host   pulumi.StringOutput
-	Scheme string // Scheme is a string as it's known in code and is useful to check HTTP/HTTPS
+	Scheme string `pulumi:"scheme"` // Scheme is a string as it's known in code and is useful to check HTTP/HTTPS
 	Port   uint32 // Same for Port
 
 	URL pulumi.StringOutput `pulumi:"url"`

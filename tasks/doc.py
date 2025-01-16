80c6293949e4acf41e920807641c0509fb013eec
Celian Raimbault <celian.raimbault@datadoghq.com>
2025-01-16 15:39:34 +0100

    [create-vm-agent-config-adxt-84] Updated doc

--- tasks/doc.py
+++ tasks/doc.py
@@ -33,4 +33,4 @@ full_image_path: str = "The full image path (registry:tag) of the Agent image to
 cluster_agent_full_image_path: str = "The full image path (registry:tag) of the Cluster Agent image to deploy"
 add_known_host: str = "Add the host to the known_hosts file (default True)"
 agent_flavor: str = "Use a specific Agent flavor (such as datadog-fips-agent, see PackageFlavor https://github.com/DataDog/agent-release-management/blob/main/generator/const.py)"
-agent_config_path: str = "Override the config for the Agent from a config file at this location"
+agent_config_path: str = "Agent config to merge with default config from a file at this location"

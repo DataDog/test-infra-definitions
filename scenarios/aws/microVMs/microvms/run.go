57a29412c1782716ef0d1804df11a1b0fe70b5aa
Celian Raimbault <celian.raimbault@datadoghq.com>
2025-01-17 13:25:16 +0100

    [create-vm-agent-config-adxt-84] Undone microvm changes

--- scenarios/aws/microVMs/microvms/run.go
+++ scenarios/aws/microVMs/microvms/run.go
@@ -150,15 +150,6 @@ func newMetalInstance(instanceEnv *InstanceEnvironment, name, arch string, m con
 			agentOptions = append(agentOptions, agentparams.WithFlavor(awsEnv.AgentFlavor()))
 		}
 
-		if awsEnv.AgentConfigPath() != "" {
-			configContent, err := awsEnv.CustomAgentConfig()
-			if err != nil {
-				return nil, err
-			}
-
-			agentOptions = append(agentOptions, agentparams.WithAgentConfig(configContent))
-		}
-
 		_, err := agent.NewHostAgent(awsEnv, awsInstance, agentOptions...)
 		if err != nil {
 			awsEnv.Ctx().Log.Warn(fmt.Sprintf("failed to deploy datadog agent on host instance: %v", err), nil)

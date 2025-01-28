6209b0f6e3ab494ca49a4c5ebd453b6749f6e76e
Guillermo Julián <guillermo.julian@datadoghq.com>
2025-01-28 19:25:31 +0100

    fix code quality violation

--- scenarios/aws/microVMs/microvms/network.go
+++ scenarios/aws/microVMs/microvms/network.go
@@ -308,7 +308,7 @@ func normalizeMAC(mac string) (string, error) {
 // waitForDHCPLeases waits for the macOS DHCP server (BootP) to assign an IP address to the VM based on its MAC address, and
 // returns that IP address.
 func waitForDHCPLeases(mac string) (string, error) {
-	mac, err := normalizeMAC(mac)
+	normalizedMac, err := normalizeMAC(mac)
 	if err != nil {
 		return "", fmt.Errorf("waitForBootpDHCPLeases: invalid MAC address: %w", err)
 	}
@@ -335,7 +335,7 @@ func waitForDHCPLeases(mac string) (string, error) {
 		leases = append(leases, arpLeases...)
 
 		for _, lease := range leases {
-			if lease.mac == mac {
+			if lease.mac == normalizedMac {
 				return lease.ip, nil
 			}
 		}

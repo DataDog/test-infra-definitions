4d94bc02d5a296c1a41b1a3ae4f969b2e292ff47
Célian Raimbault <161456554+CelianR@users.noreply.github.com>
2025-02-28 06:17:32 -0500

    Revert "Improve disable-unattended-upgrade script to also disable apt daily upgrade" (#1454)

--- components/os/scripts/apt-disable-unattended-upgrades.sh
+++ components/os/scripts/apt-disable-unattended-upgrades.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
 apt-get -y remove unattended-upgrades
-sudo systemctl disable unattented-upgrades.service
-sudo systemctl stop unattented-upgrades.service
-
-sudo systemctl disable apt-daily.service
-sudo systemctl disable apt-daily.timer
-sudo systemctl stop apt-daily.service
-sudo systemctl stop apt-daily.timer
-
-sudo systemctl disable apt-daily-upgrade.service
-sudo systemctl disable apt-daily-upgrade.timer
-sudo systemctl stop apt-daily-upgrade.service
-sudo systemctl stop apt-daily-upgrade.timer

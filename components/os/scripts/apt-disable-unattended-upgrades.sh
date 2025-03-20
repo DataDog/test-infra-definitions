37219b27b29d57eb1f9678565840a4007c95cf6b
Kevin Fairise <132568982+KevinFairise2@users.noreply.github.com>
2025-03-20 15:16:13 +0100

    Fix horrible typo in userdata script (#1480)

--- components/os/scripts/apt-disable-unattended-upgrades.sh
+++ components/os/scripts/apt-disable-unattended-upgrades.sh
@@ -2,8 +2,8 @@
 apt-get -y remove unattended-upgrades
 
 # Try to disable unattended upgrades and apt automatic updates, should not fail if it is not installed
-sudo systemctl disable unattented-upgrades.service || true
-sudo systemctl stop unattented-upgrades.service || true
+sudo systemctl disable unattended-upgrades.service || true
+sudo systemctl stop unattended-upgrades.service || true
 
 sudo systemctl disable apt-daily.service || true
 sudo systemctl disable apt-daily.time || true

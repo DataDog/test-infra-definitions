c5a3d37dc8670f7e25255badb85e1b1427a34c2d
Guillermo Julián <guillermo.julian@datadoghq.com>
2025-03-21 16:19:25 +0100

    Move kill to the top

--- components/os/scripts/apt-disable-unattended-upgrades.sh
+++ components/os/scripts/apt-disable-unattended-upgrades.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-apt-get -y remove unattended-upgrades
+
+# Kill any unattended-upgrades processes
+sudo killall -9 unattended-upgrades || true
+sudo killall -9 unattended-upgrade-shutdown || true
 
 # Try to disable unattended upgrades and apt automatic updates, should not fail if it is not installed
 sudo systemctl disable unattented-upgrades.service || true
@@ -15,6 +18,4 @@ sudo systemctl disable apt-daily-upgrade.timer || true
 sudo systemctl stop apt-daily-upgrade.service || true
 sudo systemctl stop apt-daily-upgrade.timer || true
 
-# Kill any remaining unattended-upgrades processes just in case
-sudo killall -9 unattended-upgrades || true
-sudo killall -9 unattended-upgrade-shutdown || true
+apt-get -y purge unattended-upgrades

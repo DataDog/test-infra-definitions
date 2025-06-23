7df8c990f8c234ea5cd4ffa30a680c1c038032d5
Kevin Fairise <kevin.fairise@datadoghq.com>
2025-06-23 12:20:33 +0200

    Use private copy of SLES AMI

--- scenarios/aws/ec2/os_resolver.go
+++ scenarios/aws/ec2/os_resolver.go
@@ -174,6 +174,15 @@ func resolveSuseAMI(e aws.Environment, osInfo *os.Descriptor) (string, error) {
 		osInfo.Version = os.SuseDefault.Version
 	}
 
+	if osInfo.Version == "15-sp4" {
+		if osInfo.Architecture == os.AMD64Arch {
+			return "ami-067dfda331f8296b0", nil // Private copy of the AMI dd-agent-sles-15-x86_64
+		} else if osInfo.Architecture == os.ARM64Arch {
+			return "ami-0dd7f664cbd69840b", nil // Private copy of the AMI dd-agent-sles-15-arm64
+		}
+		return "", fmt.Errorf("architecture %s is not supported for SUSE %s", osInfo.Architecture, osInfo.Version)
+	}
+
 	return ec2.GetAMIFromSSM(e, fmt.Sprintf("/aws/service/suse/sles/%s/%s/latest", osInfo.Version, osInfo.Architecture))
 }
 

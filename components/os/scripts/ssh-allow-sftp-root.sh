6b84c52ea4dac84a541d2cfc7ebbeace9b61179d
Fabrice <fabrice.buoro@datadoghq.com>
2025-09-29 11:39:28 +0200

    Fix ssh-allow-sftp-root.sh script for OS without defaultUserData (#1718)

--- components/os/scripts/ssh-allow-sftp-root.sh
+++ components/os/scripts/ssh-allow-sftp-root.sh
@@ -1,4 +1,6 @@
-# No shebang because this script will be concatenated
+#!/bin/bash
+
+set -e
 
 # Modify root's key restrictions to allow SFTP connections as root
 sed -ie 's/^.* ssh-rsa/restrict,command="internal-sftp" ssh-rsa/' /root/.ssh/authorized_keys

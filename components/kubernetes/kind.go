349d95f1ed97cbbc22c8fa96b03b418085bd2d13
Frank Spano <frank.spano@datadoghq.com>
2025-10-21 14:28:28 -0400

    check for valid version

--- components/kubernetes/kind.go
+++ components/kubernetes/kind.go
@@ -31,6 +31,20 @@ func NewKindCluster(env config.Env, vm *remote.Host, name string, kubeVersion st
 	return NewKindClusterWithConfig(env, vm, name, kubeVersion, kindClusterConfig, opts...)
 }
 
+func validateKubeVersionFormat(kubeVersion string) error {
+	// Pattern: v{semver}@sha256:{hash}
+	// Example: v1.32.0@sha256:c48c62eac5da28cdadcf560d1d8616cfa6783b58f0d94cf63ad1bf49600cb027
+	pattern := `^v\d+\.\d+\.\d+@sha256:[a-f0-9]{64}$`
+	matched, err := regexp.MatchString(pattern, kubeVersion)
+	if err != nil {
+		return fmt.Errorf("error validating kubeVersion format: %w", err)
+	}
+	if !matched {
+		return fmt.Errorf("kubeVersion must be in format 'v{semver}@sha256:{hash}' (e.g., v1.32.0@sha256:c48c62eac5da28cdadcf560d1d8616cfa6783b58f0d94cf63ad1bf49600cb027), got: %s", kubeVersion)
+	}
+	return nil
+}
+
 func NewKindClusterWithConfig(env config.Env, vm *remote.Host, name string, kubeVersion, kindConfig string, opts ...pulumi.ResourceOption) (*Cluster, error) {
 	return components.NewComponent(env, name, func(clusterComp *Cluster) error {
 		kindClusterName := env.CommonNamer().DisplayName(49) // We can have some issues if the name is longer than 50 characters
@@ -52,11 +66,18 @@ func NewKindClusterWithConfig(env config.Env, vm *remote.Host, name string, kube
 		/*
 			We'll first try to resolve the kind version and node image from our static map, if we can't find
 			it (ex. 1.34 not in our map yet), we'll continue because we have the ability to pull down arbitrary
-			versions from the mirror. It's assumed that the cluster creation will fail if we can't find the version.
+			versions from the mirror - although it should be noted the sha is required. So sometimes the version is
+			just the tag, and sometimes it's the tag with the sha.
 		*/
 		kindVersionConfig, err := GetKindVersionConfig(kubeVersion)
 		if err != nil {
 			log.Printf("[WARN] Could not find version %s in our static map, using default kind version and the provided k8s version as node image", kubeVersion)
+
+			// Validate the kubeVersion format when not in static map
+			if err := validateKubeVersionFormat(kubeVersion); err != nil {
+				return err
+			}
+
 			kindVersionConfig = &KindConfig{
 				KindVersion:      env.KindVersion(),
 				NodeImageVersion: kubeVersion,
@@ -76,8 +97,8 @@ func NewKindClusterWithConfig(env config.Env, vm *remote.Host, name string, kube
 			return err
 		}
 
-		// The internal mirror should be able to pull arbitrary kubernetes images with just the tag. ex 1.34, the sha
-		// shouldn't be necessary. We also support the user supplying the url (incase we want to host kubernetes rc
+		// The internal mirror should be able to pull arbitrary kubernetes images but the sha is required
+		//with the tag. We also support the user supplying the url (in case we want to host kubernetes rc
 		// candidates in some registry, etc)
 		var nodeImage string
 		if env.KubeNodeURL() != "" {

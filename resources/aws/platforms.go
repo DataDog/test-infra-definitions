486d7a6c66983b2005557f61d53a1fb2aff9eb7f
Célian Raimbault <161456554+CelianR@users.noreply.github.com>
2025-11-19 09:15:32 -0500

    Revert "Re-apply [ACIX-1036] Update AMIs" (#1783)

--- resources/aws/platforms.go
+++ resources/aws/platforms.go
@@ -14,46 +14,95 @@ type PlatformsArchsType = map[string]PlatformsAMIsType
 type PlatformsType = map[string]PlatformsArchsType
 
 // All the OS descriptors / AMIs correspondance
-// NOTE: Please read this documentation to update these AMIs: https://datadoghq.atlassian.net/wiki/x/04B8TgE
 var platforms = PlatformsType{
+	"debian": PlatformsArchsType{
+		"x86_64": PlatformsAMIsType{
+			"9":  "ami-0182559468c1975fe",
+			"10": "ami-0ea3c9b1a2bcdabfc",
+			"11": "ami-03039e227b7b461b5",
+			"12": "ami-0eef9d92ec044bc94",
+		},
+		"arm64": PlatformsAMIsType{
+			"10": "ami-0497a51e798361d65",
+			"11": "ami-05f3f532b93bba0b3",
+			"12": "ami-0f6dec0dab168c2f5",
+		},
+	},
+	"ubuntu": PlatformsArchsType{
+		"x86_64": PlatformsAMIsType{
+			"14-04": "ami-013d633d3b6cdb22c",
+			"16-04": "ami-0b0ea68c435eb488d",
+			"18-04": "ami-055744c75048d8296",
+			"20-04": "ami-0fb0b230890ccd1e6",
+			"22-04": "ami-090c309e8ced8ecc2",
+			"23-04": "ami-0bf964046a441a4ee",
+			"23-10": "ami-0949b45ef274e55a1",
+			"24-04": "ami-0360c520857e3138f",
+		},
+		"arm64": PlatformsAMIsType{
+			"18-04":   "ami-0fea31578248bcd6c",
+			"20-04":   "ami-0c7114fa3eac14de1",
+			"20-04-2": "ami-023f1e40b096c3ebc",
+			"21-04":   "ami-044f0ceee8e885e87",
+			"22-04":   "ami-09059bc76ad3db67f",
+			"23-04":   "ami-09b2701695676705d",
+			"23-10":   "ami-0dea732dd5f1da0a8",
+			"24-04":   "ami-026fccd88446aa0bf",
+		},
+	},
 	"amazon-linux": PlatformsArchsType{
 		"x86_64": PlatformsAMIsType{
 			"2-4-14":    "ami-038b3df3312ddf25d",
 			"2-5-10":    "ami-06a0cd9728546d178",
-			"2":         "ami-09f3869f369570aea",
 			"2022-5-15": "ami-0f0f00c2d082c52ae",
-			"2023":      "ami-0f4a4fa5d1c6e0704",
-			"2018":      "ami-0cabc8cbc2098cbb8",
+			"2023":      "ami-00ca32bbc84273381",
+			"2018":      "ami-07541a4f680f1ba8e",
+			"2":         "ami-0023921b4fcd5382b",
 		},
 		"arm64": PlatformsAMIsType{
 			"2-4-14":    "ami-090230ed0c6b13c74",
 			"2-5-10":    "ami-09e51988f56677f44",
-			"2":         "ami-032ef643b17e7d307",
 			"2022-5-15": "ami-0acc51c3357f26240",
-			"2023":      "ami-0505d2a2a44257d17",
+			"2023":      "ami-0aa7db6294d00216f",
+			"2":         "ami-00aae26e31bb072a2",
 		},
 	},
 	"amazon-linux-ecs": PlatformsArchsType{
 		"x86_64": PlatformsAMIsType{
-			"2023": "ami-0307e11f511d976b0",
-			"2":    "ami-00ca58aa4e961c973",
+			"2023": "ami-0986768eff12aa2b9",
+			"2":    "ami-0293ff221e87260aa",
 		},
 		"arm64": PlatformsAMIsType{
-			"2023": "ami-0729b1e535f19c7b8",
-			"2":    "ami-0033bec731b87c602",
+			"2023": "ami-032a0cd402947954b",
+			"2":    "ami-07af7b838076acdcc",
 		},
 	},
-	"debian": PlatformsArchsType{
+	"redhat": PlatformsArchsType{
 		"x86_64": PlatformsAMIsType{
-			"9":  "ami-0182559468c1975fe",
-			"10": "ami-0c0131c7dd91f82ea",
-			"11": "ami-0136ba2af1041319b",
-			"12": "ami-03b080214eb370bf2",
+			"9":       "ami-0c878dd49ca800252",
+			"86":      "ami-031eff1ae75bb87e4",
+			"86-fips": "ami-0d0fb96b595c56e03",
 		},
 		"arm64": PlatformsAMIsType{
-			"10": "ami-054d2bc47c1082594",
-			"11": "ami-089492faff470d87d",
-			"12": "ami-07e7ff4fc4bfb342e",
+			"9":  "ami-089b86d2f4d27cd98",
+			"86": "ami-0238411fb452f8275",
+		},
+	},
+	"suse": PlatformsArchsType{
+		"x86_64": PlatformsAMIsType{
+			"12":   "ami-0b0597153739840c4",
+			"15-4": "ami-067dfda331f8296b0",
+		},
+		"arm64": PlatformsAMIsType{
+			"15-4": "ami-08350d1d1649d8c05",
+		},
+	},
+	"fedora": PlatformsArchsType{
+		"x86_64": PlatformsAMIsType{
+			"40": "ami-004f552bba0e5f64f",
+		},
+		"arm64": PlatformsAMIsType{
+			"42": "ami-0184eee8cd4a6080b",
 		},
 	},
 	"centos": PlatformsArchsType{
@@ -65,75 +114,25 @@ var platforms = PlatformsType{
 			"79": "ami-0cb7a00afccf30559",
 		},
 	},
-	"fedora": PlatformsArchsType{
-		"x86_64": PlatformsAMIsType{
-			"40": "ami-0fac75d0443e36a79",
-		},
-		"arm64": PlatformsAMIsType{
-			"42": "ami-0b2d98d9724ad62c3",
-		},
-	},
-	"macos": PlatformsArchsType{
-		"x86_64": PlatformsAMIsType{
-			"sonoma": "ami-023abfbd427744a75",
-		},
-		"arm64": PlatformsAMIsType{
-			"sonoma": "ami-0e76a72aa5d6effe8",
-		},
-	},
-	"redhat": PlatformsArchsType{
-		"x86_64": PlatformsAMIsType{
-			"86":      "ami-00064b50696aa0436",
-			"86-fips": "ami-0d0fb96b595c56e03",
-			"9":       "ami-028e9c68b36a04979",
-		},
-		"arm64": PlatformsAMIsType{
-			"86": "ami-0d4438fbccc652f68",
-			"9":  "ami-02a1ba80018705af3",
-		},
-	},
 	"rocky-linux": PlatformsArchsType{
 		"x86_64": PlatformsAMIsType{
 			"92": "ami-08f362c39d03a4eb5",
 		},
 	},
-	"suse": PlatformsArchsType{
-		"x86_64": PlatformsAMIsType{
-			"12":   "ami-095d0932653cdb09b",
-			"15-4": "ami-025731bf74aa12ed6",
-		},
-		"arm64": PlatformsAMIsType{
-			"15-4": "ami-0fee86fe165a4b4c3",
-		},
-	},
-	"ubuntu": PlatformsArchsType{
-		"x86_64": PlatformsAMIsType{
-			"14-04": "ami-013d633d3b6cdb22c",
-			"16-04": "ami-0dd82fcb5978fb075",
-			"18-04": "ami-081753330aeb8a334",
-			"20-04": "ami-0f22bf0f32cb71fb0",
-			"22-04": "ami-031d84b460f190047",
-			"23-04": "ami-04909211b4197c028",
-			"23-10": "ami-0949b45ef274e55a1",
-			"24-04": "ami-07458119f8579729d",
-		},
-		"arm64": PlatformsAMIsType{
-			"18-04":   "ami-055744c75048d8296",
-			"20-04":   "ami-062505f473642c789",
-			"20-04-2": "ami-023f1e40b096c3ebc",
-			"21-04":   "ami-0aa5218db2b0ff1d9",
-			"22-04":   "ami-02490af8f731890a0",
-			"23-04":   "ami-0820bcaf37ee46ff4",
-			"23-10":   "ami-0dea732dd5f1da0a8",
-			"24-04":   "ami-08a72149658f1eeea",
-		},
-	},
 	"windows-server": PlatformsArchsType{
 		"x86_64": PlatformsAMIsType{
-			"2016": "ami-067445cefec7d6dc2",
-			"2019": "ami-099d25416e4c6b6d0",
-			"2022": "ami-0a1d318bc20fe9080",
-			"2025": "ami-0a9e84d3e59a24b71",
+			"2025": "ami-0efee5160a1079475",
+			"2022": "ami-028dc1123403bd543",
+			"2019": "ami-043cf96255cd85b98",
+			"2016": "ami-0fe657c1315199148",
+		},
+	},
+	"macos": PlatformsArchsType{
+		"arm64": PlatformsAMIsType{
+			"sonoma": "ami-0c582a76ed8159789",
+		},
+		"x86_64": PlatformsAMIsType{
+			"sonoma": "ami-0af4746d79fd670cd",
 		},
 	},
 }
